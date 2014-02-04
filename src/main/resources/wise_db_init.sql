434153075797f3a60b7857e92a4267aabc06c98d
hiroki terashima <honchikun@gmail.com>
2014-02-04 14:26:50 -0800

    new db init sql for mysql

--- src/main/resources/wise_db_init.sql
+++ src/main/resources/wise_db_init.sql
@@ -57,7 +57,7 @@
 
     create table announcements (
         id bigint not null auto_increment,
-        announcement text not null,
+        announcement mediumtext not null,
         timestamp datetime not null,
         title varchar(255) not null,
         primary key (id)
@@ -121,7 +121,7 @@
         primary key (group_fk, user_fk)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-    create table ideabasket (
+    create table ideaBasket (
         id bigint not null auto_increment,
         action varchar(255),
         actionPerformer bigint,
@@ -146,7 +146,7 @@
 
     create table messages (
         id bigint not null auto_increment,
-        body varchar(1024) not null,
+        body text not null,
         date datetime not null,
         subject varchar(255) not null,
         originalMessage bigint,
@@ -182,7 +182,7 @@
     create table newsitem (
         id bigint not null auto_increment,
         date datetime not null,
-        news mediumtext not null,
+        news text not null,
         title varchar(255) not null,
         owner bigint not null,
         primary key (id)
@@ -344,7 +344,7 @@
         primary key (project_fk, tag_fk)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-    create table runstatus (
+    create table runStatus (
         id bigint not null auto_increment,
         runId bigint,
         status mediumtext,
@@ -418,7 +418,7 @@
         primary key (id)
     ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-    create table studentstatus (
+    create table studentStatus (
         id bigint not null auto_increment,
         periodId bigint,
         runId bigint,
@@ -944,7 +944,7 @@ INSERT INTO user_details (id, account_not_expired, account_not_locked, credentia
 
 INSERT INTO users (id, OPTLOCK, user_details_fk) VALUES (1,0,1),(2,0,2);
 
-INSERT INTO teacher_user_details (city,country,curriculumsubjects,displayname,isEmailValid,firstname,lastlogintime,lastname,numberoflogins,schoollevel,schoolname,signupdate,state,id) VALUES ('Berkeley','USA',NULL,'adminuser',0,'ad',NULL,'min',0,3,'Berkeley','2010-10-25 15:41:31','CA',1),('Berkeley','USA',NULL,'preview',0,'pre',NULL,'view',0,3,'Berkeley','2010-10-25 15:41:31','CA',2);
+INSERT INTO teacher_user_details (city,country,curriculumsubjects,displayname,isEmailValid,firstname,lastlogintime,lastname,numberoflogins,schoollevel,schoolname,signupdate,state,id) VALUES ('Berkeley','USA',NULL,'adminuser','0','ad',NULL,'min',0,3,'Berkeley','2010-10-25 15:41:31','CA',1),('Berkeley','USA',NULL,'preview','0','pre',NULL,'view',0,3,'Berkeley','2010-10-25 15:41:31','CA',2);
 
 INSERT INTO user_details_related_to_roles VALUES (1,1),(1,2),(1,3),(1,5),(2,1),(2,3),(2,5);
 
