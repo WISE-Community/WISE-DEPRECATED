5a5c2242c572bfb8d76b094512eceb7811dc90fb
hiroki terashima <honchikun@gmail.com>
2014-02-04 13:50:17 -0800

    fixed path for network firewall check .swf and .jar assets

--- src/main/webapp/portal/javascript/checkCompatibility.js
+++ src/main/webapp/portal/javascript/checkCompatibility.js
@@ -627,7 +627,7 @@ function DetectFlashVer(reqMajorVer, reqMinorVer, reqRevision)
 function checkContentFiltering() {
 	// test loading of swf file
 	$.ajax({ 
-		url: "../flash/convection-intro.swf", 
+		url: "../portal/flash/convection-intro.swf", 
 		context: document.body})
 		.success(function(data, textStatus, jqXHR) {
 			var contentFilterSwfRequirementSatisfied=false;
@@ -648,7 +648,7 @@ function checkContentFiltering() {
 
 	// test loading of jar file
 	var jqxhr = $.ajax({ 
-		url: "../library/jar/commons-logging-1.1.jar", 
+		url: "../portal/library/jar/commons-logging-1.1.jar", 
 		context: document.body})
 		.success(function(data, textStatus, jqXHR) {			
 			var contentFilterRequirementSatisfied=false;
