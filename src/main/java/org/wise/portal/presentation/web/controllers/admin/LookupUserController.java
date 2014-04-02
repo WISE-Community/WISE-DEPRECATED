eecdc79d88113b564f43ab89ea9a0a106189cc9b
hiroki terashima <honchikun@gmail.com>
2014-04-02 13:12:14 -0700

    Merged LookupTeacherController and LookupStudentController into LookupUserController. Renamed LookupParameters toLookupUserParameters. Renamed LookupParametersValidator toLookupUserParametersValidator.

---
+++ src/main/java/org/wise/portal/presentation/web/controllers/admin/LookupUserController.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2008-2014 Regents of the University of California (Regents). Created
+ * by TELS, Graduate School of Education, University of California at Berkeley.
+ *
+ * This software is distributed under the GNU General Public License, v3.
+ *
+ * Permission is hereby granted, without written agreement and without license
+ * or royalty fees, to use, copy, modify, and distribute this software and its
+ * documentation for any purpose, provided that the above copyright notice and
+ * the following two paragraphs appear in all copies of this software.
+ *
+ * REGENTS SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE. THE SOFTWAREAND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED
+ * HEREUNDER IS PROVIDED "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE
+ * MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS.
+ *
+ * IN NO EVENT SHALL REGENTS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT,
+ * SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES, INCLUDING LOST PROFITS,
+ * ARISING OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF
+ * REGENTS HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+ */
+package org.wise.portal.presentation.web.controllers.admin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.validation.BindException;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.SimpleFormController;
+import org.wise.portal.domain.admin.StudentFields;
+import org.wise.portal.domain.admin.TeacherFields;
+import org.wise.portal.domain.authentication.Curriculumsubjects;
+import org.wise.portal.domain.authentication.Gender;
+import org.wise.portal.domain.authentication.Schoollevel;
+import org.wise.portal.domain.impl.LookupUserParameters;
+import org.wise.portal.domain.user.User;
+import org.wise.portal.service.user.UserService;
+
+/**
+ * @author Patrick Lawler
+ * @author Hiroki Terashima
+ * @version $Id:$
+ */
+public class LookupUserController extends SimpleFormController {
+	
+	private UserService userService;
+
+	@Override
+	protected Map<String, Object> referenceData(HttpServletRequest request) throws Exception {
+		Map<String, Object> model = new HashMap<String, Object>();
+		String userType = request.getParameter("userType");
+		model.put("userType", userType);
+		if ("teacher".equals(userType)) {
+			model.put("fields", TeacherFields.values());
+		} else {
+			model.put("fields", StudentFields.values());
+		}
+		return model;
+	}
+
+	/**
+     * @see org.springframework.web.servlet.mvc.SimpleFormController#onSubmit(javax.servlet.http.HttpServletRequest,
+     *      javax.servlet.http.HttpServletResponse, java.lang.Object,
+     *      org.springframework.validation.BindException)
+     */
+	@Override
+	protected ModelAndView onSubmit(HttpServletRequest request,
+            HttpServletResponse response, Object command, BindException errors){
+		LookupUserParameters params = (LookupUserParameters) command;
+		Object term = new Object();
+		
+		// if searching for ID, make the term object in a Long.
+		if ("ID".equals(params.getLookupField())) {
+			term = Long.parseLong(params.getLookupData());
+		} else if(params.getLookupField().equals("GENDER")){
+			term = Gender.valueOf(params.getLookupData().toUpperCase());
+		} else if(params.getLookupCriteria().equals("like")){
+			term = "%" + params.getLookupData() + "%";
+		} else if(params.getLookupField().equals("SCHOOLLEVEL")){
+			term = getLevel(params.getLookupData());
+		} else if(params.getLookupCriteria().equals("like")) {
+			term = "%" + params.getLookupData() + "%";
+		} else {
+			term = params.getLookupData();
+		}
+		
+		String userDetailsType = "teacherUserDetails";
+		if ("student".equals(request.getParameter("userType"))) {
+			userDetailsType = "studentUserDetails";
+		}
+		
+		List<User> users = this.userService.retrieveByField(params.getLookupField()
+				.toLowerCase(),	params.getLookupCriteria(), term,
+				userDetailsType);
+		
+		ModelAndView modelAndView = new ModelAndView("admin/account/manageusers");
+		// put the usernames in an array
+		List<String> usernames = new ArrayList<String>();
+		for (User user : users) {
+			usernames.add(user.getUserDetails().getUsername());
+		}
+		
+		if(users.size() < 1){
+			modelAndView.addObject("message", "No users given search criteria found.");
+		} else {
+			if ("student".equals(request.getParameter("userType"))) {
+				modelAndView.addObject("students", usernames);
+			} else {
+				modelAndView.addObject("teachers", usernames);
+			}
+		}
+		
+		return modelAndView;
+	}
+
+	private Schoollevel getLevel(String level){
+		for(Schoollevel schoolLevel : Schoollevel.values()){
+			if(schoolLevel.toString().toUpperCase().contains(level.toUpperCase()))
+				return schoolLevel;
+		}
+		return Schoollevel.OTHER;
+	}
+	
+	/**
+	 * @param userService the userService to set
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+}
