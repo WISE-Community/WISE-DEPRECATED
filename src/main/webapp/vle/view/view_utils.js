83f627f698cef57d3edacd63fc8a9f1e4ea7afcf
hiroki terashima <honchikun@gmail.com>
2014-10-24 10:36:06 -0700

    Replace all instances of {{studentFirstNames}} WISE variable, not just the first.

--- src/main/webapp/vle/view/view_utils.js
+++ src/main/webapp/vle/view/view_utils.js
@@ -2456,7 +2456,7 @@ View.prototype.replaceWISEVariables = function(text) {
 		var studentFirstNamesArray = this.getUserAndClassInfo().getStudentFirstNamesByWorkgroupId(workgroupId);
 		var studentFirstNames = studentFirstNamesArray.join(' & ');
 		
-		text = text.replace("{{studentFirstNames}}", studentFirstNames);
+		text = text.replace(/{{studentFirstNames}}/g, studentFirstNames);
 	} 
 	return text;
 };
