e45bddde272a2227657188cce002acf3a9b606a5
hiroki terashima <honchikun@gmail.com>
2015-03-01 15:09:43 -0800

    Added authoring to PhET node

--- src/main/webapp/nodes/phet/phet.js
+++ src/main/webapp/nodes/phet/phet.js
@@ -1,10 +1,52 @@
+var nodeContentIsDirty = false;
+
 function getStepState() {
         return null;
 }
 
 function setContent(content) {
-    $('#phetIFrame').attr('src',content.url);
+    if (mode === 'author') {
+        $('#phetSimURL').html(content.url);
+        $('#phetSimURL').change(function() {
+            nodeContentIsDirty = true;
+            $('#saveNodeContentButton').attr('disabled', false);
+        });
+    } else {
+        $('#phetIFrame').attr('src', content.url);
+    }
 }
 
 function setStepState(stepState) {
 }
+
+function saveNodeContent(callback, callbackArgs) {
+    if (mode === 'author') {
+        var phetSimURL = $('#phetSimURL').val();
+        var nodeContentJSON = {};
+        nodeContentJSON.url = phetSimURL; 
+        saveNodeContentToWISE(nodeContentJSON, callback, callbackArgs);
+    }   
+}
+
+function authorViewOnSimulationSelected(phetSimURL) {
+    $('#phetSimURL').val(phetSimURL);
+    $('#phetSimURL').change();
+}
+
+$(document).ready(function() {
+    if (mode === 'author') {
+        $('#studentView').hide();
+        $('#authorView').show(); 
+        $('#saveNodeContentButton').click(function() {
+            saveNodeContent(function() {
+                $('#saveNodeContentButton').attr('disabled', true);
+                nodeContentIsDirty = false;
+            });
+        });
+        loadWISEData();
+    } else {
+        $('#authorView').hide();
+        $('#studentView').show();  
+        loadWISEData({loadAllNodeStates:true});
+    }
+});
