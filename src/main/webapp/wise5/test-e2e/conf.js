b87296c550f4a6f232088cc73b87684647ddc429
Hiroki Terashima <honchikun@gmail.com>
2016-03-31 15:27:42 -0700

    SauceLabs Protractor Travis CI E2E integration test attempt #1.

--- src/main/webapp/wise5/test-e2e/conf.js
+++ src/main/webapp/wise5/test-e2e/conf.js
@@ -6,3 +6,14 @@ exports.config = {
         'authoringTool.spec.js'
     ]
 };
+
+// This won’t affect local tests, it will only activate when running on Travis CI:
+if (process.env.TRAVIS) {
+    config.sauceUser = process.env.SAUCE_USERNAME;
+    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    config.capabilities = {
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER
+    };
+}
