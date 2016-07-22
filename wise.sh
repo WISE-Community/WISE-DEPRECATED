5c182734066eb4fe0637709b68b1c0f8c0507fb2
Hiroki Terashima <honchikun@gmail.com>
2016-07-22 16:35:18 -0700

    Changed wise.sh so that you'd run with param "run" all the time (including very first time) and only run "reset" if you want to wipe out data. This would make it easier to remember for most use cases.

--- wise.sh
+++ wise.sh
@@ -7,50 +7,55 @@ PWD=`pwd`
 PROPERTIES_FILE="src/main/resources/wise.properties"
 SAMPLE_PROPERTIES_FILE="src/main/resources/wise_sample_embedded_tomcat.properties"
 
-if [ $# -eq 0 ]
+if [ \( $# -eq 0 \) -o \( $1 != "reset" -a $1 != "run" \) ]
 then
-    echo "Usage: ./wise.sh {setup|run}"
-else
-    if [ $1 = "setup" ]
-    then
-        # install npm dependencies
-        npm install
-
-        # install jspm dependencies
-        cd src/main/webapp/wise5
-        npm install jspm
-        jspm install
-        cd -
-
-	# clear out curriculum and student uploads directories and any existing properties file
-	find src/main/webapp/curriculum/ ! \( -name README -o -name .gitignore \) -type d \( -path demo \) -delete
-	find src/main/webapp/studentuploads/ ! \( -name README -o -name .gitignore \) -delete
-	rm $PROPERTIES_FILE
-
-	# copy sample property file and set paths automatically
-	cp $SAMPLE_PROPERTIES_FILE $PROPERTIES_FILE
-        sed -i.bak '/wiseBaseDir=.*/d' $PROPERTIES_FILE
-	echo "wiseBaseDir=$PWD/src/main/webapp/" >> $PROPERTIES_FILE
-        sed -i.bak '/curriculum_base_dir=.*/d' $PROPERTIES_FILE
-	echo "curriculum_base_dir=$PWD/src/main/webapp/curriculum" >> $PROPERTIES_FILE
-        sed -i.bak '/studentuploads_base_dir=.*/d' $PROPERTIES_FILE
-	echo "studentuploads_base_dir=$PWD/src/main/webapp/studentuploads" >> $PROPERTIES_FILE
-
-        # prepare to recreate db tables
-        sed -i.bak '/hibernate.hbm2ddl.auto=[none|create]/d' $PROPERTIES_FILE
-        echo "hibernate.hbm2ddl.auto=create" >> $PROPERTIES_FILE
-
-        # start embedded tomcat
-        mvn clean compile tomcat7:run
-    else
-        if [ $1 = "run" ]
-        then
-            # make sure db tables are not wiped out
-            sed -i.bak '/hibernate.hbm2ddl.auto=[none|create]/d' $PROPERTIES_FILE
-            echo "hibernate.hbm2ddl.auto=none" >> $PROPERTIES_FILE
-
-            # start embedded tomcat
-            mvn clean compile tomcat7:run
-        fi
-    fi
+    echo "Usage: ./wise.sh {reset|run}"
+    exit 0
 fi
+
+
+if [ $1 = "reset" ]
+then
+  # clear out curriculum and student uploads directories and any existing properties file
+  find src/main/webapp/curriculum/ ! \( -name README -o -name .gitignore \) -type d \( -path demo \) -delete
+  find src/main/webapp/studentuploads/ ! \( -name README -o -name .gitignore \) -delete
+  rm $PROPERTIES_FILE
+else
+  if [ ! -f $PROPERTIES_FILE ]
+  then
+    # properties file does not exist, assume this is a fresh install
+    # install npm dependencies
+    npm install
+
+    # install jspm dependencies
+    cd src/main/webapp/wise5
+    npm install jspm
+    jspm install
+    cd -
+
+    # copy sample property file and set paths automatically
+    cp $SAMPLE_PROPERTIES_FILE $PROPERTIES_FILE
+    sed -i.bak '/wiseBaseDir=.*/d' $PROPERTIES_FILE
+    echo "wiseBaseDir=$PWD/src/main/webapp/" >> $PROPERTIES_FILE
+    sed -i.bak '/curriculum_base_dir=.*/d' $PROPERTIES_FILE
+    echo "curriculum_base_dir=$PWD/src/main/webapp/curriculum" >> $PROPERTIES_FILE
+    sed -i.bak '/studentuploads_base_dir=.*/d' $PROPERTIES_FILE
+    echo "studentuploads_base_dir=$PWD/src/main/webapp/studentuploads" >> $PROPERTIES_FILE
+
+    # prepare to recreate db tables
+    sed -i.bak '/hibernate.hbm2ddl.auto=[none|create]/d' $PROPERTIES_FILE
+    echo "hibernate.hbm2ddl.auto=create" >> $PROPERTIES_FILE
+
+    # start embedded tomcat
+    mvn clean compile tomcat7:run
+
+  else
+    # make sure db tables are not wiped out
+    sed -i.bak '/hibernate.hbm2ddl.auto=[none|create]/d' $PROPERTIES_FILE
+    echo "hibernate.hbm2ddl.auto=none" >> $PROPERTIES_FILE
+
+    # start embedded tomcat
+    mvn clean compile tomcat7:run
+  fi
+fi
+
