c4fa6e77dd4a3596b6838be5a9f679247fb64bac
breity <breity10@gmail.com>
2016-09-03 11:42:34 -0700

    Updated the component annotations display in the vle (student view): new style, differentiates between teacher and automated feedback, changes color and adds label when new
    - Also added some minor style updates to the vle

--- src/main/webapp/wise5/directives/componentAnnotations/componentAnnotationsController.es6
+++ src/main/webapp/wise5/directives/componentAnnotations/componentAnnotationsController.es6
@@ -3,35 +3,190 @@
 class ComponentAnnotationsController {
     constructor($scope,
                 $element,
+                $translate,
+                ConfigService,
                 ProjectService,
-                $mdMedia) {
+                StudentDataService) {
+        this.$scope = $scope;
+        this.$translate = $translate;
+        this.ConfigService = ConfigService;
         this.ProjectService = ProjectService;
+        this.StudentDataService = StudentDataService;
 
-        // the score annotation
-        //this.score = this.scoreAnnotation ? this.scoreAnnotation.data.value : null;
-        //this.hasScore = (parseInt(this.score) > -1);
         this.maxScoreDisplay = (parseInt(this.maxScore) > 0) ? '/' + this.maxScore : '';
-
-        // the comment annotation
-        //this.comment = this.commentAnnotation ? this.commentAnnotation.data.value : null;
 
         this.themeSettings = this.ProjectService.getThemeSettings();
         this.hideComponentScores = this.themeSettings.hideComponentScores;
 
-        // watch for browser window width changes and set smScreen to true when applicable
-        $scope.$watch(() => { return $mdMedia('gt-xs'); }, (sm) => {
-            this.smScreen = sm;
+        this.nodeId = null;
+        this.componentId = null;
+
+        // the latest annoation time
+        this.latestAnnotationTime = null;
+
+        // whether the annotation is new or not
+        this.isNew = false;
+
+        // the annotation label
+        this.label = '';
+
+        // the avatar icon (default to person/teacher)
+        this.icon = 'person';
+
+        // watch for new component states
+        this.$scope.$on('studentWorkSavedToServer', (event, args) => {
+            let nodeId = args.studentWork.nodeId;
+            let componentId = args.studentWork.componentId;
+            if (nodeId === this.nodeId && componentId === this.componentId) {
+                this.isNew = false;
+            }
         });
-        
-        // TODO: differentiate between teacher, automated, and peer annotations in the future
+
+        this.$onChanges = (changes) => {
+            this.processAnnotations();
+        };
     }
+
+    /**
+     * Get the most recent annotation (from the current score and comment annotations)
+     * @return Object (latest annotation)
+     */
+    getLatestAnnotation() {
+        let latest = null;
+
+        if (this.commentAnnotation || this.scoreAnnotation) {
+            let commentSaveTime = this.commentAnnotation ? this.commentAnnotation.serverSaveTime : 0;
+            let scoreSaveTime = this.scoreAnnotation ? this.scoreAnnotation.serverSaveTime : 0;
+
+            if (commentSaveTime >= scoreSaveTime) {
+                latest = this.commentAnnotation;
+            } else if (scoreSaveTime > commentSaveTime) {
+                latest = this.scoreAnnotation;
+            }
+        }
+
+        return latest;
+    };
+
+    /**
+     * Calculate the save time of the latest annotation
+     * @return Number (latest annotation post time)
+     */
+    getLatestAnnotationTime() {
+        let latest = this.getLatestAnnotation();
+        let time = null;
+
+        if (latest) {
+            let serverSaveTime = latest.serverSaveTime;
+            time = this.ConfigService.convertToClientTimestamp(serverSaveTime)
+        }
+
+        return time;
+    };
+
+    /**
+     * Find nodeExited time of the latest node visit for this component
+     * @return Number (latest node exit time)
+     */
+    getLatestVisitTime() {
+        let nodeEvents = this.StudentDataService.getEventsByNodeId(this.nodeId);
+        let n = nodeEvents.length - 1;
+        let visitTime = null;
+
+        for (let i = n; i > 0; i--) {
+            let event = nodeEvents[i];
+            if (event.event === 'nodeExited') {
+                visitTime = this.ConfigService.convertToClientTimestamp(event.serverSaveTime);
+                break;
+            }
+        }
+
+        return visitTime;
+    };
+
+    /**
+     * Find and the latest save time for this component
+     * @return Number (latest save time)
+     */
+    getLatestSaveTime() {
+        let latestState = this.StudentDataService.getLatestComponentStateByNodeIdAndComponentId(this.nodeId, this.componentId);
+        let saveTime = null;
+
+        if (latestState) {
+            saveTime = this.ConfigService.convertToClientTimestamp(latestState.serverSaveTime);
+        }
+
+        return saveTime;
+    };
+
+    /**
+     * Check whether the current annotation for this component is new to the
+     * workgroup (i.e. if the workgroup hasn't seen the annotation on a previous
+     * node visit and the latest annotation came after the latest component state)
+     * @return Boolean (true or false)
+     */
+    isNewAnnotation() {
+        let latestVisitTime = this.getLatestVisitTime();
+        let latestSaveTime = this.getLatestSaveTime();
+        let latestAnnotationTime = this.getLatestAnnotationTime();
+        let isNew = true;
+
+        if (latestVisitTime && (latestVisitTime > latestAnnotationTime)) {
+            isNew = false;
+        }
+
+        if (latestSaveTime && (latestSaveTime > latestAnnotationTime)) {
+            isNew = false;
+        }
+
+        return isNew;
+    };
+
+    /**
+     * Set the label based on whether this is an automated or teacher annotation
+     **/
+    setLabelAndIcon() {
+        let latest = this.getLatestAnnotation();
+
+        if (latest) {
+            if (latest.type === 'autoComment' || latest.type === 'autoScore') {
+                this.$translate(['automatedFeedbackLabel']).then((translations) => {
+                    this.label = translations.automatedFeedbackLabel;
+                    this.icon = 'keyboard';
+                });
+            } else {
+                this.$translate(['teacherFeedbackLabel']).then((translations) => {
+                    this.label = translations.teacherFeedbackLabel;
+                    this.icon = "person";
+                });
+            }
+        }
+    };
+
+    processAnnotations() {
+        if (this.commentAnnotation || this.scoreAnnotation) {
+            this.nodeId = this.commentAnnotation ? this.commentAnnotation.nodeId : this.scoreAnnotation.nodeId;
+            this.componentId = this.commentAnnotation ? this.commentAnnotation.componentId : this.scoreAnnotation.nodeId;
+
+            // set the latest annotation time
+            //this.latestAnnotationTime = this.getLatestAnnotationTime();
+
+            // set whether the annotation is new or not
+            //this.isNew = this.isNewAnnotation();
+
+            // set the annotation label and icon
+            this.setLabelAndIcon();
+        }
+    };
 }
 
 ComponentAnnotationsController.$inject = [
     '$scope',
     '$element',
+    '$translate',
+    'ConfigService',
     'ProjectService',
-    '$mdMedia'
+    'StudentDataService'
 ];
 
 export default ComponentAnnotationsController;
--- src/main/webapp/wise5/directives/componentAnnotations/componentAnnotationsController.js
+++ src/main/webapp/wise5/directives/componentAnnotations/componentAnnotationsController.js
@@ -4,37 +4,215 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var ComponentAnnotationsController = function ComponentAnnotationsController($scope, $element, ProjectService, $mdMedia) {
-    var _this = this;
+var ComponentAnnotationsController = function () {
+    function ComponentAnnotationsController($scope, $element, $translate, ConfigService, ProjectService, StudentDataService) {
+        var _this = this;
 
-    _classCallCheck(this, ComponentAnnotationsController);
+        _classCallCheck(this, ComponentAnnotationsController);
 
-    this.ProjectService = ProjectService;
+        this.$scope = $scope;
+        this.$translate = $translate;
+        this.ConfigService = ConfigService;
+        this.ProjectService = ProjectService;
+        this.StudentDataService = StudentDataService;
 
-    // the score annotation
-    //this.score = this.scoreAnnotation ? this.scoreAnnotation.data.value : null;
-    //this.hasScore = (parseInt(this.score) > -1);
-    this.maxScoreDisplay = parseInt(this.maxScore) > 0 ? '/' + this.maxScore : '';
+        this.maxScoreDisplay = parseInt(this.maxScore) > 0 ? '/' + this.maxScore : '';
 
-    // the comment annotation
-    //this.comment = this.commentAnnotation ? this.commentAnnotation.data.value : null;
+        this.themeSettings = this.ProjectService.getThemeSettings();
+        this.hideComponentScores = this.themeSettings.hideComponentScores;
 
-    this.themeSettings = this.ProjectService.getThemeSettings();
-    this.hideComponentScores = this.themeSettings.hideComponentScores;
+        this.nodeId = null;
+        this.componentId = null;
 
-    // watch for browser window width changes and set smScreen to true when applicable
-    $scope.$watch(function () {
-        return $mdMedia('gt-xs');
-    }, function (sm) {
-        _this.smScreen = sm;
-    });
+        // the latest annoation time
+        this.latestAnnotationTime = null;
 
-    // TODO: differentiate between teacher, automated, and peer annotations in the future
-};
+        // whether the annotation is new or not
+        this.isNew = false;
 
-ComponentAnnotationsController.$inject = ['$scope', '$element', 'ProjectService', '$mdMedia'];
+        // the annotation label
+        this.label = '';
+
+        // the avatar icon (default to person/teacher)
+        this.icon = 'person';
+
+        // watch for new component states
+        this.$scope.$on('studentWorkSavedToServer', function (event, args) {
+            var nodeId = args.studentWork.nodeId;
+            var componentId = args.studentWork.componentId;
+            if (nodeId === _this.nodeId && componentId === _this.componentId) {
+                _this.isNew = false;
+            }
+        });
+
+        this.$onChanges = function (changes) {
+            _this.processAnnotations();
+        };
+    }
+
+    /**
+     * Get the most recent annotation (from the current score and comment annotations)
+     * @return Object (latest annotation)
+     */
+
+
+    _createClass(ComponentAnnotationsController, [{
+        key: 'getLatestAnnotation',
+        value: function getLatestAnnotation() {
+            var latest = null;
+
+            if (this.commentAnnotation || this.scoreAnnotation) {
+                var commentSaveTime = this.commentAnnotation ? this.commentAnnotation.serverSaveTime : 0;
+                var scoreSaveTime = this.scoreAnnotation ? this.scoreAnnotation.serverSaveTime : 0;
+
+                if (commentSaveTime >= scoreSaveTime) {
+                    latest = this.commentAnnotation;
+                } else if (scoreSaveTime > commentSaveTime) {
+                    latest = this.scoreAnnotation;
+                }
+            }
+
+            return latest;
+        }
+    }, {
+        key: 'getLatestAnnotationTime',
+
+
+        /**
+         * Calculate the save time of the latest annotation
+         * @return Number (latest annotation post time)
+         */
+        value: function getLatestAnnotationTime() {
+            var latest = this.getLatestAnnotation();
+            var time = null;
+
+            if (latest) {
+                var serverSaveTime = latest.serverSaveTime;
+                time = this.ConfigService.convertToClientTimestamp(serverSaveTime);
+            }
+
+            return time;
+        }
+    }, {
+        key: 'getLatestVisitTime',
+
+
+        /**
+         * Find nodeExited time of the latest node visit for this component
+         * @return Number (latest node exit time)
+         */
+        value: function getLatestVisitTime() {
+            var nodeEvents = this.StudentDataService.getEventsByNodeId(this.nodeId);
+            var n = nodeEvents.length - 1;
+            var visitTime = null;
+
+            for (var i = n; i > 0; i--) {
+                var event = nodeEvents[i];
+                if (event.event === 'nodeExited') {
+                    visitTime = this.ConfigService.convertToClientTimestamp(event.serverSaveTime);
+                    break;
+                }
+            }
+
+            return visitTime;
+        }
+    }, {
+        key: 'getLatestSaveTime',
+
+
+        /**
+         * Find and the latest save time for this component
+         * @return Number (latest save time)
+         */
+        value: function getLatestSaveTime() {
+            var latestState = this.StudentDataService.getLatestComponentStateByNodeIdAndComponentId(this.nodeId, this.componentId);
+            var saveTime = null;
+
+            if (latestState) {
+                saveTime = this.ConfigService.convertToClientTimestamp(latestState.serverSaveTime);
+            }
+
+            return saveTime;
+        }
+    }, {
+        key: 'isNewAnnotation',
+
+
+        /**
+         * Check whether the current annotation for this component is new to the
+         * workgroup (i.e. if the workgroup hasn't seen the annotation on a previous
+         * node visit and the latest annotation came after the latest component state)
+         * @return Boolean (true or false)
+         */
+        value: function isNewAnnotation() {
+            var latestVisitTime = this.getLatestVisitTime();
+            var latestSaveTime = this.getLatestSaveTime();
+            var latestAnnotationTime = this.getLatestAnnotationTime();
+            var isNew = true;
+
+            if (latestVisitTime && latestVisitTime > latestAnnotationTime) {
+                isNew = false;
+            }
+
+            if (latestSaveTime && latestSaveTime > latestAnnotationTime) {
+                isNew = false;
+            }
+
+            return isNew;
+        }
+    }, {
+        key: 'setLabelAndIcon',
+
+
+        /**
+         * Set the label based on whether this is an automated or teacher annotation
+         **/
+        value: function setLabelAndIcon() {
+            var _this2 = this;
+
+            var latest = this.getLatestAnnotation();
+
+            if (latest) {
+                if (latest.type === 'autoComment' || latest.type === 'autoScore') {
+                    this.$translate(['automatedFeedbackLabel']).then(function (translations) {
+                        _this2.label = translations.automatedFeedbackLabel;
+                        _this2.icon = 'keyboard';
+                    });
+                } else {
+                    this.$translate(['teacherFeedbackLabel']).then(function (translations) {
+                        _this2.label = translations.teacherFeedbackLabel;
+                        _this2.icon = "person";
+                    });
+                }
+            }
+        }
+    }, {
+        key: 'processAnnotations',
+        value: function processAnnotations() {
+            if (this.commentAnnotation || this.scoreAnnotation) {
+                this.nodeId = this.commentAnnotation ? this.commentAnnotation.nodeId : this.scoreAnnotation.nodeId;
+                this.componentId = this.commentAnnotation ? this.commentAnnotation.componentId : this.scoreAnnotation.nodeId;
+
+                // set the latest annotation time
+                //this.latestAnnotationTime = this.getLatestAnnotationTime();
+
+                // set whether the annotation is new or not
+                //this.isNew = this.isNewAnnotation();
+
+                // set the annotation label and icon
+                this.setLabelAndIcon();
+            }
+        }
+    }]);
+
+    return ComponentAnnotationsController;
+}();
+
+ComponentAnnotationsController.$inject = ['$scope', '$element', '$translate', 'ConfigService', 'ProjectService', 'StudentDataService'];
 
 exports.default = ComponentAnnotationsController;
 //# sourceMappingURL=componentAnnotationsController.js.map
