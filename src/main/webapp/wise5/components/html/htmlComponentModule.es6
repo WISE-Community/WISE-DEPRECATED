495efb7abf99492155c8da8a0d3fe812461f3dd7
Geoffrey Kwan <geoffreykwan@gmail.com>
2017-01-09 15:02:10 -0800

    HTML Component: Added English translation entries.

--- src/main/webapp/wise5/components/html/htmlComponentModule.es6
+++ src/main/webapp/wise5/components/html/htmlComponentModule.es6
@@ -5,6 +5,12 @@ import HTMLController from './htmlController';
 
 let htmlComponentModule = angular.module('htmlComponentModule', [])
     .service(HTMLService.name, HTMLService)
-    .controller(HTMLController.name, HTMLController);
+    .controller(HTMLController.name, HTMLController)
+    .config([
+        '$translatePartialLoaderProvider',
+        ($translatePartialLoaderProvider) => {
+            $translatePartialLoaderProvider.addPart('components/html/i18n');
+        }
+    ]);
 
 export default htmlComponentModule;
--- src/main/webapp/wise5/components/html/htmlComponentModule.js
+++ src/main/webapp/wise5/components/html/htmlComponentModule.js
@@ -14,7 +14,9 @@ var _htmlController2 = _interopRequireDefault(_htmlController);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var htmlComponentModule = angular.module('htmlComponentModule', []).service(_htmlService2.default.name, _htmlService2.default).controller(_htmlController2.default.name, _htmlController2.default);
+var htmlComponentModule = angular.module('htmlComponentModule', []).service(_htmlService2.default.name, _htmlService2.default).controller(_htmlController2.default.name, _htmlController2.default).config(['$translatePartialLoaderProvider', function ($translatePartialLoaderProvider) {
+    $translatePartialLoaderProvider.addPart('components/html/i18n');
+}]);
 
 exports.default = htmlComponentModule;
 //# sourceMappingURL=htmlComponentModule.js.map
