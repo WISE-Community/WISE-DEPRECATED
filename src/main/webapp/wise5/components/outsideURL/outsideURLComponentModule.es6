4ed98798bfbe8c1629688f0bb08fbbf303fda644
Geoffrey Kwan <geoffreykwan@gmail.com>
2017-01-09 14:53:44 -0800

    Outside URL Component: Added English translation entries.

--- src/main/webapp/wise5/components/outsideURL/outsideURLComponentModule.es6
+++ src/main/webapp/wise5/components/outsideURL/outsideURLComponentModule.es6
@@ -5,6 +5,12 @@ import OutsideURLController from './outsideURLController';
 
 let outsideURLComponentModule = angular.module('outsideURLComponentModule', [])
     .service(OutsideURLService.name, OutsideURLService)
-    .controller(OutsideURLController.name, OutsideURLController);
+    .controller(OutsideURLController.name, OutsideURLController)
+    .config([
+        '$translatePartialLoaderProvider',
+        ($translatePartialLoaderProvider) => {
+            $translatePartialLoaderProvider.addPart('components/outsideURL/i18n');
+        }
+    ]);
 
 export default outsideURLComponentModule;
--- src/main/webapp/wise5/components/outsideURL/outsideURLComponentModule.js
+++ src/main/webapp/wise5/components/outsideURL/outsideURLComponentModule.js
@@ -14,7 +14,9 @@ var _outsideURLController2 = _interopRequireDefault(_outsideURLController);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var outsideURLComponentModule = angular.module('outsideURLComponentModule', []).service(_outsideURLService2.default.name, _outsideURLService2.default).controller(_outsideURLController2.default.name, _outsideURLController2.default);
+var outsideURLComponentModule = angular.module('outsideURLComponentModule', []).service(_outsideURLService2.default.name, _outsideURLService2.default).controller(_outsideURLController2.default.name, _outsideURLController2.default).config(['$translatePartialLoaderProvider', function ($translatePartialLoaderProvider) {
+    $translatePartialLoaderProvider.addPart('components/outsideURL/i18n');
+}]);
 
 exports.default = outsideURLComponentModule;
 //# sourceMappingURL=outsideURLComponentModule.js.map
