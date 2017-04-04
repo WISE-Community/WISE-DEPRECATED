5190d6c891e38272f14e73437f81dee237acbab8
Geoffrey Kwan <geoffreykwan@gmail.com>
2017-04-03 18:41:10 -0700

    Embedded Component: Models can now specify isCompleted by setting the isCompleted field in the student data that gets saved to WISE.

--- src/main/webapp/wise5/components/embedded/embeddedService.js
+++ src/main/webapp/wise5/components/embedded/embeddedService.js
@@ -77,17 +77,63 @@ var EmbeddedService = function (_NodeService) {
         value: function isCompleted(component, componentStates, componentEvents, nodeEvents) {
             var result = false;
 
-            if (nodeEvents != null) {
+            var isCompletedFieldInComponentState = false;
 
-                // loop through all the events
-                for (var e = 0; e < nodeEvents.length; e++) {
+            if (componentStates != null) {
 
-                    // get an event
-                    var event = nodeEvents[e];
+                /*
+                 * loop through all the component states and look for a component
+                 * that has the isCompleted field set to true
+                 */
+                for (var c = 0; c < componentStates.length; c++) {
 
-                    if (event != null && event.event === 'nodeEntered') {
-                        result = true;
-                        break;
+                    // get a component state
+                    var componentState = componentStates[c];
+
+                    if (componentState != null) {
+                        // get the student data from the model
+                        var studentData = componentState.studentData;
+
+                        if (studentData != null) {
+
+                            if (studentData.isCompleted != null) {
+                                /*
+                                 * the model has set the isCompleted field in the
+                                 * student data
+                                 */
+                                isCompletedFieldInComponentState = true;
+
+                                if (studentData.isCompleted === true) {
+                                    /*
+                                     * the model has set the isCompleted field to true
+                                     * which means the student has completed the component
+                                     */
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (isCompletedFieldInComponentState == false) {
+                /*
+                 * the isCompleted field was not set into the component state so
+                 * we will look for events to determine isCompleted
+                 */
+
+                if (nodeEvents != null) {
+
+                    // loop through all the events
+                    for (var e = 0; e < nodeEvents.length; e++) {
+
+                        // get an event
+                        var event = nodeEvents[e];
+
+                        if (event != null && event.event === 'nodeEntered') {
+                            result = true;
+                            break;
+                        }
                     }
                 }
             }
