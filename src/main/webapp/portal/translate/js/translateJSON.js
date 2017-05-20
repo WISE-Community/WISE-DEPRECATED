70bf665894a6046fc46802ad1f361563d0d202aa
Hiroki Terashima <honchikun@gmail.com>
2017-05-19 18:56:51 -0700

    Translation: addedSlashes to default values and show them in an read-only textarea. This should now work correctly for values with breaks and quotes.

--- src/main/webapp/portal/translate/js/translateJSON.js
+++ src/main/webapp/portal/translate/js/translateJSON.js
@@ -28,8 +28,8 @@ function buildTable5() {
 	// build the rest of the table
 	if (currentLanguage != "") {        
 		for (key in View.prototype.i18n[View.prototype.i18n.defaultLocale]) {
-			var value = View.prototype.i18n[View.prototype.i18n.defaultLocale][key];
-			translationTable += "<tr class='translationRow'>\n<td class='cell_key'>"+key+"</td>\n<td>"+value+"</td>\n";
+			var value = addSlashes(View.prototype.i18n[View.prototype.i18n.defaultLocale][key]);
+			translationTable += "<tr class='translationRow'>\n<td class='cell_key'>"+key+"</td>\n<td><textarea style='border:none;width:100%;height:100%' readonly='true'>"+value+"</textarea></td>\n";
 			if (View.prototype.i18n[currentLanguage][key]) {
 				translationTable += "<td class='cell_currentLanguage'><textarea style='height:100%;width:100%' id='"+key+"'>"+View.prototype.i18n[currentLanguage][key]+"</textarea></td>\n";
 			} else {
@@ -77,7 +77,8 @@ function buildTable_JSON() {
 	if (currentLanguage != "") {        
 		for (key in View.prototype.i18n[View.prototype.i18n.defaultLocale]) {
 			var obj = View.prototype.i18n[View.prototype.i18n.defaultLocale][key];
-			translationTable += "<tr class='translationRow'>\n<td class='cell_key'>"+key+"</td>\n<td>"+obj.description+"</td>\n<td>"+obj.value+"</td>\n";
+			var value = addSlashes(obj.value);
+			translationTable += "<tr class='translationRow'>\n<td class='cell_key'>"+key+"</td>\n<td>"+obj.description+"</td>\n<td><textarea style='border:none;width:100%;height:100%' readonly='true'>"+value+"</textarea></td>\n";
 			if (View.prototype.i18n[currentLanguage][key]) {
 				translationTable += "<td class='cell_currentLanguage'><textarea style='height:100%;width:100%' id='"+key+"'>"+View.prototype.i18n[currentLanguage][key].value+"</textarea></td>\n";
 			} else {
