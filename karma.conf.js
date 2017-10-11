880edd32305284df52b7a7d355da0f0b9008e474
Hiroki Terashima <honchikun@gmail.com>
2017-10-11 16:40:18 -0700

    Show more spec information in karma unit tests using karma-spec-runner. Fixes #941. run npm install after pulling.

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function(config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['spec'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
