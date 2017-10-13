WISE-Community/WISE-DEPRECATED

commit 31a6cbb1b633100ea18edb6ab137256febcded2f

Classroom Monitor: Moved period select to the global toolbar and out of individual views
- Updated some styles for select elements/buttons in toolbars

src/main/webapp/wise5/classroomMonitor/notebook/notebookGradingController.es6
@@ -61,16 +61,6 @@ class NotebookGradingController {
             this.canGradeStudentWork = false;
         }
 
-        this.periods = this.ConfigService.getPeriods();
-
-        // set the current period if it hasn't been set yet
-        if (this.getCurrentPeriod() == null) {
-            if (this.periods != null && this.periods.length > 0) {
-                // set it to the all periods option
-                this.setCurrentPeriod(this.periods[0]);
-            }
-        }
-
         // save event when notebook grading view is displayed
         let context = "ClassroomMonitor", nodeId = null, componentId = null, componentType = null,
             category = "Navigation", event = "notebookViewDisplayed", data = {};
@@ -116,22 +106,6 @@ class NotebookGradingController {
     viewReport(workgroupId) {
         alert(workgroupId);
     }
-
-    /**
-     * Get the current period
-     */
-    getCurrentPeriod() {
-        return this.TeacherDataService.getCurrentPeriod();
-    };
-
-    /**
-     * Set the current period
-     * @param period the period object
-     */
-    setCurrentPeriod(period) {
-        this.TeacherDataService.setCurrentPeriod(period);
-        this.$rootScope.$broadcast('periodChanged', {period: period});
-    };
 
 }
 

src/main/webapp/wise5/classroomMonitor/notebook/notebookGradingController.js
@@ -62,16 +62,6 @@ var NotebookGradingController = function () {
             this.canGradeStudentWork = false;
         }
 
-        this.periods = this.ConfigService.getPeriods();
-
-        // set the current period if it hasn't been set yet
-        if (this.getCurrentPeriod() == null) {
-            if (this.periods != null && this.periods.length > 0) {
-                // set it to the all periods option
-                this.setCurrentPeriod(this.periods[0]);
-            }
-        }
-
         // save event when notebook grading view is displayed
         var context = "ClassroomMonitor",
             nodeId = null,
@@ -132,28 +122,6 @@ var NotebookGradingController = function () {
         key: "viewReport",
         value: function viewReport(workgroupId) {
             alert(workgroupId);
-        }
-
-        /**
-         * Get the current period
-         */
-
-    }, {
-        key: "getCurrentPeriod",
-        value: function getCurrentPeriod() {
-            return this.TeacherDataService.getCurrentPeriod();
-        }
-    }, {
-        key: "setCurrentPeriod",
-
-
-        /**
-         * Set the current period
-         * @param period the period object
-         */
-        value: function setCurrentPeriod(period) {
-            this.TeacherDataService.setCurrentPeriod(period);
-            this.$rootScope.$broadcast('periodChanged', { period: period });
         }
     }]);
 

commit 794afa93f4566be054558184dd218ac10a0f58df

Classroom Monitor: Fixed a bug introduced in 31a6cbb that prevented the Notebook view from showing any workgroups

src/main/webapp/wise5/classroomMonitor/notebook/notebookGradingController.es6
@@ -107,6 +107,13 @@ class NotebookGradingController {
         alert(workgroupId);
     }
 
+    /**
+     * Get the current period
+     */
+    getCurrentPeriod() {
+        return this.TeacherDataService.getCurrentPeriod();
+    };
+
 }
 
 NotebookGradingController.$inject = [

src/main/webapp/wise5/classroomMonitor/notebook/notebookGradingController.js
@@ -123,6 +123,16 @@ var NotebookGradingController = function () {
         value: function viewReport(workgroupId) {
             alert(workgroupId);
         }
+
+        /**
+         * Get the current period
+         */
+
+    }, {
+        key: "getCurrentPeriod",
+        value: function getCurrentPeriod() {
+            return this.TeacherDataService.getCurrentPeriod();
+        }
     }]);
 
     return NotebookGradingController;

src/main/webapp/wise5/classroomMonitor/notebook/notebookGradingController.js.map
@@ -1,1 +1,1 @@
-{"version":3,"sources":["notebookGradingController.es6"],"names":["NotebookGradingController","$rootScope","$scope","$state","ConfigService","NotebookService","ProjectService","StudentStatusService","TeacherDataService","TeacherWebSocketService","themePath","getThemePath","teacherWorkgroupId","getWorkgroupId","workgroups","getClassmateUserInfos","noteFilter","reportFilter","showAllNotes","showAllReports","showNoteForWorkgroup","showReportForWorkgroup","i","length","workgroup","workgroupId","canViewStudentNames","canGradeStudentWork","role","getTeacherRole","context","nodeId","componentId","componentType","category","event","data","saveEvent","alert","$inject"],"mappings":"AAAA;;;;;;;;;;IAEMA,yB;AAEF,uCAAYC,UAAZ,EACYC,MADZ,EAEYC,MAFZ,EAGYC,aAHZ,EAIYC,eAJZ,EAKYC,cALZ,EAMYC,oBANZ,EAOYC,kBAPZ,EAQYC,uBARZ,EAQqC;AAAA;;AACjC,aAAKR,UAAL,GAAkBA,UAAlB;AACA,aAAKC,MAAL,GAAcA,MAAd;AACA,aAAKC,MAAL,GAAcA,MAAd;AACA,aAAKC,aAAL,GAAqBA,aAArB;AACA,aAAKC,eAAL,GAAuBA,eAAvB;AACA,aAAKC,cAAL,GAAsBA,cAAtB;AACA,aAAKC,oBAAL,GAA4BA,oBAA5B;AACA,aAAKC,kBAAL,GAA0BA,kBAA1B;AACA,aAAKC,uBAAL,GAA+BA,uBAA/B;;AAEA,aAAKC,SAAL,GAAiB,KAAKJ,cAAL,CAAoBK,YAApB,EAAjB;;AAEA,aAAKC,kBAAL,GAA0B,KAAKR,aAAL,CAAmBS,cAAnB,EAA1B;;AAEA;AACA,aAAKC,UAAL,GAAkB,KAAKV,aAAL,CAAmBW,qBAAnB,EAAlB;;AAEA,aAAKC,UAAL,GAAkB,MAAlB;AACA,aAAKC,YAAL,GAAoB,QAApB;;AAEA,aAAKC,YAAL,GAAoB,KAApB;AACA,aAAKC,cAAL,GAAsB,KAAtB;AACA,aAAKC,oBAAL,GAA4B,EAA5B;AACA,aAAKC,sBAAL,GAA8B,EAA9B;AACA,aAAK,IAAIC,IAAI,CAAb,EAAgBA,IAAI,KAAKR,UAAL,CAAgBS,MAApC,EAA4CD,GAA5C,EAAiD;AAC7C,gBAAIE,YAAY,KAAKV,UAAL,CAAgBQ,CAAhB,CAAhB;AACA,iBAAKF,oBAAL,CAA0BI,UAAUC,WAApC,IAAmD,KAAnD;AACA,iBAAKJ,sBAAL,CAA4BG,UAAUC,WAAtC,IAAqD,KAArD;AACH;;AAED,aAAKC,mBAAL,GAA2B,IAA3B;AACA,aAAKC,mBAAL,GAA2B,IAA3B;;AAEA;AACA,YAAIC,OAAO,KAAKxB,aAAL,CAAmByB,cAAnB,CAAkC,KAAKjB,kBAAvC,CAAX;;AAEA,YAAIgB,SAAS,OAAb,EAAsB;AAClB;AACA,iBAAKF,mBAAL,GAA2B,IAA3B;AACA,iBAAKC,mBAAL,GAA2B,IAA3B;AACH,SAJD,MAIO,IAAIC,SAAS,OAAb,EAAsB;AACzB;AACA,iBAAKF,mBAAL,GAA2B,IAA3B;AACA,iBAAKC,mBAAL,GAA2B,IAA3B;AACH,SAJM,MAIA,IAAIC,SAAS,MAAb,EAAqB;AACxB;AACA,iBAAKF,mBAAL,GAA2B,KAA3B;AACA,iBAAKC,mBAAL,GAA2B,KAA3B;AACH;;AAED;AACA,YAAIG,UAAU,kBAAd;AAAA,YAAkCC,SAAS,IAA3C;AAAA,YAAiDC,cAAc,IAA/D;AAAA,YAAqEC,gBAAgB,IAArF;AAAA,YACIC,WAAW,YADf;AAAA,YAC6BC,QAAQ,uBADrC;AAAA,YAC8DC,OAAO,EADrE;AAEA,aAAK5B,kBAAL,CAAwB6B,SAAxB,CAAkCP,OAAlC,EAA2CC,MAA3C,EAAmDC,WAAnD,EAAgEC,aAAhE,EAA+EC,QAA/E,EAAyFC,KAAzF,EAAgGC,IAAhG;AACH;;;;sDAE6BX,W,EAAa;AACvC,iBAAKL,oBAAL,CAA0BK,WAA1B,IAAyC,CAAC,KAAKL,oBAAL,CAA0BK,WAA1B,CAA1C;AACH;;;wDAE+BA,W,EAAa;AACzC,iBAAKJ,sBAAL,CAA4BI,WAA5B,IAA2C,CAAC,KAAKJ,sBAAL,CAA4BI,WAA5B,CAA5C;AACH;;;gDAEuB;AACpB,iBAAKP,YAAL,GAAoB,CAAC,KAAKA,YAA1B;;AAEA,iBAAK,IAAIO,WAAT,IAAwB,KAAKL,oBAA7B,EAAmD;AAC/C,qBAAKA,oBAAL,CAA0BK,WAA1B,IAAyC,KAAKP,YAA9C;AACH;AACJ;;;kDAEyB;AACtB,iBAAKC,cAAL,GAAsB,CAAC,KAAKA,cAA5B;;AAEA,iBAAK,IAAIM,WAAT,IAAwB,KAAKJ,sBAA7B,EAAqD;AACjD,qBAAKA,sBAAL,CAA4BI,WAA5B,IAA2C,KAAKN,cAAhD;AACH;AACJ;;AAED;;;;;;;kCAIUM,W,EAAa;AACnBa,kBAAMb,WAAN;AACH;;AAED;;;;;;;mCAIWA,W,EAAa;AACpBa,kBAAMb,WAAN;AACH;;;;;;AAILzB,0BAA0BuC,OAA1B,GAAoC,CAChC,YADgC,EAEhC,QAFgC,EAGhC,QAHgC,EAIhC,eAJgC,EAKhC,iBALgC,EAMhC,gBANgC,EAOhC,sBAPgC,EAQhC,oBARgC,EAShC,yBATgC,CAApC;;kBAYevC,yB","file":"notebookGradingController.js","sourcesContent":["'use strict';\n\nclass NotebookGradingController {\n\n    constructor($rootScope,\n                $scope,\n                $state,\n                ConfigService,\n                NotebookService,\n                ProjectService,\n                StudentStatusService,\n                TeacherDataService,\n                TeacherWebSocketService) {\n        this.$rootScope = $rootScope;\n        this.$scope = $scope;\n        this.$state = $state;\n        this.ConfigService = ConfigService;\n        this.NotebookService = NotebookService;\n        this.ProjectService = ProjectService;\n        this.StudentStatusService = StudentStatusService;\n        this.TeacherDataService = TeacherDataService;\n        this.TeacherWebSocketService = TeacherWebSocketService;\n\n        this.themePath = this.ProjectService.getThemePath();\n\n        this.teacherWorkgroupId = this.ConfigService.getWorkgroupId();\n\n        // get the workgroups sorted alphabetically\n        this.workgroups = this.ConfigService.getClassmateUserInfos();\n\n        this.noteFilter = \"note\";\n        this.reportFilter = \"report\";\n\n        this.showAllNotes = false;\n        this.showAllReports = false;\n        this.showNoteForWorkgroup = {};\n        this.showReportForWorkgroup = {};\n        for (let i = 0; i < this.workgroups.length; i++) {\n            let workgroup = this.workgroups[i];\n            this.showNoteForWorkgroup[workgroup.workgroupId] = false;\n            this.showReportForWorkgroup[workgroup.workgroupId] = false;\n        }\n\n        this.canViewStudentNames = true;\n        this.canGradeStudentWork = true;\n\n        // get the role of the teacher for the run e.g. 'owner', 'write', 'read'\n        let role = this.ConfigService.getTeacherRole(this.teacherWorkgroupId);\n\n        if (role === 'owner') {\n            // the teacher is the owner of the run and has full access\n            this.canViewStudentNames = true;\n            this.canGradeStudentWork = true;\n        } else if (role === 'write') {\n            // the teacher is a shared teacher that can grade the student work\n            this.canViewStudentNames = true;\n            this.canGradeStudentWork = true;\n        } else if (role === 'read') {\n            // the teacher is a shared teacher that can only view the student work\n            this.canViewStudentNames = false;\n            this.canGradeStudentWork = false;\n        }\n\n        // save event when notebook grading view is displayed\n        let context = \"ClassroomMonitor\", nodeId = null, componentId = null, componentType = null,\n            category = \"Navigation\", event = \"notebookViewDisplayed\", data = {};\n        this.TeacherDataService.saveEvent(context, nodeId, componentId, componentType, category, event, data);\n    }\n\n    toggleDisplayNoteForWorkgroup(workgroupId) {\n        this.showNoteForWorkgroup[workgroupId] = !this.showNoteForWorkgroup[workgroupId];\n    }\n\n    toggleDisplayReportForWorkgroup(workgroupId) {\n        this.showReportForWorkgroup[workgroupId] = !this.showReportForWorkgroup[workgroupId];\n    }\n\n    toggleDisplayAllNotes() {\n        this.showAllNotes = !this.showAllNotes;\n\n        for (let workgroupId in this.showNoteForWorkgroup) {\n            this.showNoteForWorkgroup[workgroupId] = this.showAllNotes;\n        }\n    }\n\n    toggleDisplayAllReports() {\n        this.showAllReports = !this.showAllReports;\n\n        for (let workgroupId in this.showReportForWorkgroup) {\n            this.showReportForWorkgroup[workgroupId] = this.showAllReports;\n        }\n    }\n\n    /**\n     * Handle request to view notes for the specified workgroup\n     * @param workgroupId\n     */\n    viewNotes(workgroupId) {\n        alert(workgroupId);\n    }\n\n    /**\n     * Handle request to view report for the specified workgroup\n     * @param workgroupId\n     */\n    viewReport(workgroupId) {\n        alert(workgroupId);\n    }\n\n}\n\nNotebookGradingController.$inject = [\n    '$rootScope',\n    '$scope',\n    '$state',\n    'ConfigService',\n    'NotebookService',\n    'ProjectService',\n    'StudentStatusService',\n    'TeacherDataService',\n    'TeacherWebSocketService'\n];\n\nexport default NotebookGradingController;\n"]}
+{"version":3,"sources":["notebookGradingController.es6"],"names":["NotebookGradingController","$rootScope","$scope","$state","ConfigService","NotebookService","ProjectService","StudentStatusService","TeacherDataService","TeacherWebSocketService","themePath","getThemePath","teacherWorkgroupId","getWorkgroupId","workgroups","getClassmateUserInfos","noteFilter","reportFilter","showAllNotes","showAllReports","showNoteForWorkgroup","showReportForWorkgroup","i","length","workgroup","workgroupId","canViewStudentNames","canGradeStudentWork","role","getTeacherRole","context","nodeId","componentId","componentType","category","event","data","saveEvent","alert","getCurrentPeriod","$inject"],"mappings":"AAAA;;;;;;;;;;IAEMA,yB;AAEF,uCAAYC,UAAZ,EACYC,MADZ,EAEYC,MAFZ,EAGYC,aAHZ,EAIYC,eAJZ,EAKYC,cALZ,EAMYC,oBANZ,EAOYC,kBAPZ,EAQYC,uBARZ,EAQqC;AAAA;;AACjC,aAAKR,UAAL,GAAkBA,UAAlB;AACA,aAAKC,MAAL,GAAcA,MAAd;AACA,aAAKC,MAAL,GAAcA,MAAd;AACA,aAAKC,aAAL,GAAqBA,aAArB;AACA,aAAKC,eAAL,GAAuBA,eAAvB;AACA,aAAKC,cAAL,GAAsBA,cAAtB;AACA,aAAKC,oBAAL,GAA4BA,oBAA5B;AACA,aAAKC,kBAAL,GAA0BA,kBAA1B;AACA,aAAKC,uBAAL,GAA+BA,uBAA/B;;AAEA,aAAKC,SAAL,GAAiB,KAAKJ,cAAL,CAAoBK,YAApB,EAAjB;;AAEA,aAAKC,kBAAL,GAA0B,KAAKR,aAAL,CAAmBS,cAAnB,EAA1B;;AAEA;AACA,aAAKC,UAAL,GAAkB,KAAKV,aAAL,CAAmBW,qBAAnB,EAAlB;;AAEA,aAAKC,UAAL,GAAkB,MAAlB;AACA,aAAKC,YAAL,GAAoB,QAApB;;AAEA,aAAKC,YAAL,GAAoB,KAApB;AACA,aAAKC,cAAL,GAAsB,KAAtB;AACA,aAAKC,oBAAL,GAA4B,EAA5B;AACA,aAAKC,sBAAL,GAA8B,EAA9B;AACA,aAAK,IAAIC,IAAI,CAAb,EAAgBA,IAAI,KAAKR,UAAL,CAAgBS,MAApC,EAA4CD,GAA5C,EAAiD;AAC7C,gBAAIE,YAAY,KAAKV,UAAL,CAAgBQ,CAAhB,CAAhB;AACA,iBAAKF,oBAAL,CAA0BI,UAAUC,WAApC,IAAmD,KAAnD;AACA,iBAAKJ,sBAAL,CAA4BG,UAAUC,WAAtC,IAAqD,KAArD;AACH;;AAED,aAAKC,mBAAL,GAA2B,IAA3B;AACA,aAAKC,mBAAL,GAA2B,IAA3B;;AAEA;AACA,YAAIC,OAAO,KAAKxB,aAAL,CAAmByB,cAAnB,CAAkC,KAAKjB,kBAAvC,CAAX;;AAEA,YAAIgB,SAAS,OAAb,EAAsB;AAClB;AACA,iBAAKF,mBAAL,GAA2B,IAA3B;AACA,iBAAKC,mBAAL,GAA2B,IAA3B;AACH,SAJD,MAIO,IAAIC,SAAS,OAAb,EAAsB;AACzB;AACA,iBAAKF,mBAAL,GAA2B,IAA3B;AACA,iBAAKC,mBAAL,GAA2B,IAA3B;AACH,SAJM,MAIA,IAAIC,SAAS,MAAb,EAAqB;AACxB;AACA,iBAAKF,mBAAL,GAA2B,KAA3B;AACA,iBAAKC,mBAAL,GAA2B,KAA3B;AACH;;AAED;AACA,YAAIG,UAAU,kBAAd;AAAA,YAAkCC,SAAS,IAA3C;AAAA,YAAiDC,cAAc,IAA/D;AAAA,YAAqEC,gBAAgB,IAArF;AAAA,YACIC,WAAW,YADf;AAAA,YAC6BC,QAAQ,uBADrC;AAAA,YAC8DC,OAAO,EADrE;AAEA,aAAK5B,kBAAL,CAAwB6B,SAAxB,CAAkCP,OAAlC,EAA2CC,MAA3C,EAAmDC,WAAnD,EAAgEC,aAAhE,EAA+EC,QAA/E,EAAyFC,KAAzF,EAAgGC,IAAhG;AACH;;;;sDAE6BX,W,EAAa;AACvC,iBAAKL,oBAAL,CAA0BK,WAA1B,IAAyC,CAAC,KAAKL,oBAAL,CAA0BK,WAA1B,CAA1C;AACH;;;wDAE+BA,W,EAAa;AACzC,iBAAKJ,sBAAL,CAA4BI,WAA5B,IAA2C,CAAC,KAAKJ,sBAAL,CAA4BI,WAA5B,CAA5C;AACH;;;gDAEuB;AACpB,iBAAKP,YAAL,GAAoB,CAAC,KAAKA,YAA1B;;AAEA,iBAAK,IAAIO,WAAT,IAAwB,KAAKL,oBAA7B,EAAmD;AAC/C,qBAAKA,oBAAL,CAA0BK,WAA1B,IAAyC,KAAKP,YAA9C;AACH;AACJ;;;kDAEyB;AACtB,iBAAKC,cAAL,GAAsB,CAAC,KAAKA,cAA5B;;AAEA,iBAAK,IAAIM,WAAT,IAAwB,KAAKJ,sBAA7B,EAAqD;AACjD,qBAAKA,sBAAL,CAA4BI,WAA5B,IAA2C,KAAKN,cAAhD;AACH;AACJ;;AAED;;;;;;;kCAIUM,W,EAAa;AACnBa,kBAAMb,WAAN;AACH;;AAED;;;;;;;mCAIWA,W,EAAa;AACpBa,kBAAMb,WAAN;AACH;;AAED;;;;;;2CAGmB;AACf,mBAAO,KAAKjB,kBAAL,CAAwB+B,gBAAxB,EAAP;AACH;;;;;;AAILvC,0BAA0BwC,OAA1B,GAAoC,CAChC,YADgC,EAEhC,QAFgC,EAGhC,QAHgC,EAIhC,eAJgC,EAKhC,iBALgC,EAMhC,gBANgC,EAOhC,sBAPgC,EAQhC,oBARgC,EAShC,yBATgC,CAApC;;kBAYexC,yB","file":"notebookGradingController.js","sourcesContent":["'use strict';\r\n\r\nclass NotebookGradingController {\r\n\r\n    constructor($rootScope,\r\n                $scope,\r\n                $state,\r\n                ConfigService,\r\n                NotebookService,\r\n                ProjectService,\r\n                StudentStatusService,\r\n                TeacherDataService,\r\n                TeacherWebSocketService) {\r\n        this.$rootScope = $rootScope;\r\n        this.$scope = $scope;\r\n        this.$state = $state;\r\n        this.ConfigService = ConfigService;\r\n        this.NotebookService = NotebookService;\r\n        this.ProjectService = ProjectService;\r\n        this.StudentStatusService = StudentStatusService;\r\n        this.TeacherDataService = TeacherDataService;\r\n        this.TeacherWebSocketService = TeacherWebSocketService;\r\n\r\n        this.themePath = this.ProjectService.getThemePath();\r\n\r\n        this.teacherWorkgroupId = this.ConfigService.getWorkgroupId();\r\n\r\n        // get the workgroups sorted alphabetically\r\n        this.workgroups = this.ConfigService.getClassmateUserInfos();\r\n\r\n        this.noteFilter = \"note\";\r\n        this.reportFilter = \"report\";\r\n\r\n        this.showAllNotes = false;\r\n        this.showAllReports = false;\r\n        this.showNoteForWorkgroup = {};\r\n        this.showReportForWorkgroup = {};\r\n        for (let i = 0; i < this.workgroups.length; i++) {\r\n            let workgroup = this.workgroups[i];\r\n            this.showNoteForWorkgroup[workgroup.workgroupId] = false;\r\n            this.showReportForWorkgroup[workgroup.workgroupId] = false;\r\n        }\r\n\r\n        this.canViewStudentNames = true;\r\n        this.canGradeStudentWork = true;\r\n\r\n        // get the role of the teacher for the run e.g. 'owner', 'write', 'read'\r\n        let role = this.ConfigService.getTeacherRole(this.teacherWorkgroupId);\r\n\r\n        if (role === 'owner') {\r\n            // the teacher is the owner of the run and has full access\r\n            this.canViewStudentNames = true;\r\n            this.canGradeStudentWork = true;\r\n        } else if (role === 'write') {\r\n            // the teacher is a shared teacher that can grade the student work\r\n            this.canViewStudentNames = true;\r\n            this.canGradeStudentWork = true;\r\n        } else if (role === 'read') {\r\n            // the teacher is a shared teacher that can only view the student work\r\n            this.canViewStudentNames = false;\r\n            this.canGradeStudentWork = false;\r\n        }\r\n\r\n        // save event when notebook grading view is displayed\r\n        let context = \"ClassroomMonitor\", nodeId = null, componentId = null, componentType = null,\r\n            category = \"Navigation\", event = \"notebookViewDisplayed\", data = {};\r\n        this.TeacherDataService.saveEvent(context, nodeId, componentId, componentType, category, event, data);\r\n    }\r\n\r\n    toggleDisplayNoteForWorkgroup(workgroupId) {\r\n        this.showNoteForWorkgroup[workgroupId] = !this.showNoteForWorkgroup[workgroupId];\r\n    }\r\n\r\n    toggleDisplayReportForWorkgroup(workgroupId) {\r\n        this.showReportForWorkgroup[workgroupId] = !this.showReportForWorkgroup[workgroupId];\r\n    }\r\n\r\n    toggleDisplayAllNotes() {\r\n        this.showAllNotes = !this.showAllNotes;\r\n\r\n        for (let workgroupId in this.showNoteForWorkgroup) {\r\n            this.showNoteForWorkgroup[workgroupId] = this.showAllNotes;\r\n        }\r\n    }\r\n\r\n    toggleDisplayAllReports() {\r\n        this.showAllReports = !this.showAllReports;\r\n\r\n        for (let workgroupId in this.showReportForWorkgroup) {\r\n            this.showReportForWorkgroup[workgroupId] = this.showAllReports;\r\n        }\r\n    }\r\n\r\n    /**\r\n     * Handle request to view notes for the specified workgroup\r\n     * @param workgroupId\r\n     */\r\n    viewNotes(workgroupId) {\r\n        alert(workgroupId);\r\n    }\r\n\r\n    /**\r\n     * Handle request to view report for the specified workgroup\r\n     * @param workgroupId\r\n     */\r\n    viewReport(workgroupId) {\r\n        alert(workgroupId);\r\n    }\r\n\r\n    /**\r\n     * Get the current period\r\n     */\r\n    getCurrentPeriod() {\r\n        return this.TeacherDataService.getCurrentPeriod();\r\n    };\r\n\r\n}\r\n\r\nNotebookGradingController.$inject = [\r\n    '$rootScope',\r\n    '$scope',\r\n    '$state',\r\n    'ConfigService',\r\n    'NotebookService',\r\n    'ProjectService',\r\n    'StudentStatusService',\r\n    'TeacherDataService',\r\n    'TeacherWebSocketService'\r\n];\r\n\r\nexport default NotebookGradingController;\r\n"]}