302928ecff26a158172447dd1a1742a6b3cdb92f
Hiroki Terashima <honchikun@gmail.com>
2017-12-20 12:04:31 -0800

    Organized unit tests.

--- src/main/webapp/wise5/test-unit/services/utilService.spec.es6
+++ src/main/webapp/wise5/test-unit/services/utilService.spec.es6
@@ -2,29 +2,31 @@ import angular from 'angular';
 import mainModule from 'vle/main';
 import 'angular-mocks';
 
-describe('UtilService Unit Test', () => {
-  
+describe('UtilService', () => {
+
   beforeEach(angular.mock.module(mainModule.name));
 
   let UtilService;
+  
   beforeEach(inject((_UtilService_) => {
     UtilService = _UtilService_;
   }));
 
-  describe('UtilService', () => {
-    // Test UtilService.generateKey()
-    it('should return random keys', () => {
-      // Calling generate key with no params should return length 10 string
+  describe('generateKey()', () => {
+    it('should return random keys of length 10 by default', () => {
       const generatedKey1 = UtilService.generateKey();
       const generatedKey2 = UtilService.generateKey();
       expect(generatedKey1.length).toEqual(10);
       expect(generatedKey2.length).toEqual(10);
       expect(generatedKey1).not.toEqual(generatedKey2);
+    });
 
-      // Calling generate key with length key should produce key with specified length
-      const genetatedKey3 = UtilService.generateKey(5);
-      expect(genetatedKey3.length).toEqual(5);
+    it('should return random keys of specified length', () => {
+      expect(UtilService.generateKey(5).length).toEqual(5);
+      expect(UtilService.generateKey(23).length).toEqual(23);
+    });
 
+    it('should produce 100 unique random strings', () => {
       // Calling generate key 100 times should produce 100 unique random strings
       const generatedKeysSoFar = [];
       for (let i = 0; i < 100; i++) {
@@ -33,23 +35,21 @@ describe('UtilService Unit Test', () => {
         generatedKeysSoFar.push(generatedKey);
       }
     });
+  });
 
-    // Test UtilService.convertStringToNumber()
-    it('should convert a string to a number', () => {
-      // Calling it with a number string should return the number
+  describe('convertStringToNumber()', () => {
+    it('should convert a number string to a number', () => {
       expect(UtilService.convertStringToNumber("5")).toEqual(5);
-
-      // Calling it with a number string should return the number
       expect(UtilService.convertStringToNumber("-100")).toEqual(-100);
+    });
 
-      // Calling it with null should return null
+    it('should return null for null argument', () => {
       expect(UtilService.convertStringToNumber(null)).toBeNull();
+    });
 
-      // Calling it with a non-number string should return that non-number
+    it('should return non-null number string as is', () => {
       expect(UtilService.convertStringToNumber("abc")).toEqual("abc");
-
-      // Calling it with a non-number string should return that non-number
       expect(UtilService.convertStringToNumber("")).toEqual("");
     });
-  });
+  })
 });
--- src/main/webapp/wise5/test-unit/services/utilService.spec.js
+++ src/main/webapp/wise5/test-unit/services/utilService.spec.js
@@ -12,29 +12,31 @@ require('angular-mocks');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-describe('UtilService Unit Test', function () {
+describe('UtilService', function () {
 
   beforeEach(_angular2.default.mock.module(_main2.default.name));
 
   var UtilService = void 0;
+
   beforeEach(inject(function (_UtilService_) {
     UtilService = _UtilService_;
   }));
 
-  describe('UtilService', function () {
-    // Test UtilService.generateKey()
-    it('should return random keys', function () {
-      // Calling generate key with no params should return length 10 string
+  describe('generateKey()', function () {
+    it('should return random keys of length 10 by default', function () {
       var generatedKey1 = UtilService.generateKey();
       var generatedKey2 = UtilService.generateKey();
       expect(generatedKey1.length).toEqual(10);
       expect(generatedKey2.length).toEqual(10);
       expect(generatedKey1).not.toEqual(generatedKey2);
+    });
 
-      // Calling generate key with length key should produce key with specified length
-      var genetatedKey3 = UtilService.generateKey(5);
-      expect(genetatedKey3.length).toEqual(5);
+    it('should return random keys of specified length', function () {
+      expect(UtilService.generateKey(5).length).toEqual(5);
+      expect(UtilService.generateKey(23).length).toEqual(23);
+    });
 
+    it('should produce 100 unique random strings', function () {
       // Calling generate key 100 times should produce 100 unique random strings
       var generatedKeysSoFar = [];
       for (var i = 0; i < 100; i++) {
@@ -43,22 +45,20 @@ describe('UtilService Unit Test', function () {
         generatedKeysSoFar.push(generatedKey);
       }
     });
+  });
 
-    // Test UtilService.convertStringToNumber()
-    it('should convert a string to a number', function () {
-      // Calling it with a number string should return the number
+  describe('convertStringToNumber()', function () {
+    it('should convert a number string to a number', function () {
       expect(UtilService.convertStringToNumber("5")).toEqual(5);
-
-      // Calling it with a number string should return the number
       expect(UtilService.convertStringToNumber("-100")).toEqual(-100);
+    });
 
-      // Calling it with null should return null
+    it('should return null for null argument', function () {
       expect(UtilService.convertStringToNumber(null)).toBeNull();
+    });
 
-      // Calling it with a non-number string should return that non-number
+    it('should return non-null number string as is', function () {
       expect(UtilService.convertStringToNumber("abc")).toEqual("abc");
-
-      // Calling it with a non-number string should return that non-number
       expect(UtilService.convertStringToNumber("")).toEqual("");
     });
   });
