3c8738ffe86c92abb3e1ba88cf6dd88f524aa5c6
Jonathan Lim-Breitbart <breity10@gmail.com>
2018-01-29 10:48:07 -0800

    Site (new portal): Set angular-cli to use SCSS for styles and added initial stylesheets structure; added 'watch-all-site' script that generates CSS sourcemaps to package.json and renamed 'watch-all' to 'watch-all-wise5'

--- src/main/webapp/site/src/app/app.module.ts
+++ src/main/webapp/site/src/app/app.module.ts
@@ -5,16 +5,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './/app-routing.module';
-import { AboutComponent } from './about/about.component';
-import { NewsComponent } from './news/news.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AboutComponent,
-    NewsComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
