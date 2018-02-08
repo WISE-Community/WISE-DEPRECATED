6eed46b2a3c06e69758b6acf32e94e06ae767711
Jonathan Lim-Breitbart <breity10@gmail.com>
2018-02-08 14:46:53 -0800

    Site: Added @angular/material and @angular/flex-layout dependencies and create a default Angular Material theme plus additional WISE colors

--- src/main/webapp/site/src/app/app.module.ts
+++ src/main/webapp/site/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material.module';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { StudentModule } from './student/student.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,7 +20,10 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    FlexLayoutModule,
+    MaterialModule,
     StudentModule,
     HttpClientModule
   ],
---
+++ src/main/webapp/site/src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    MatButtonModule
+  ],
+  exports: [
+    MatButtonModule
+  ],
+})
+export class MaterialModule { }
