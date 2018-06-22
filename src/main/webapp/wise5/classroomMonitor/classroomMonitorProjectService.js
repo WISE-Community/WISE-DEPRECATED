46d87052c384722101a2271f0ecc5af0b943f49d
Geoffrey Kwan <geoffreykwan@gmail.com>
2018-06-22 15:47:23 -0700

    Removed usage of showPreviousWork. Closes #1324

--- src/main/webapp/wise5/classroomMonitor/classroomMonitorProjectService.js
+++ src/main/webapp/wise5/classroomMonitor/classroomMonitorProjectService.js
@@ -81,62 +81,6 @@ var ClassroomMonitorProjectService = function (_ProjectService) {
     }
 
     /**
-     * Get the show previous work node ids and component ids in a node
-     * @param nodeId get the show previous work node ids and component ids in
-     * this node
-     * @returns an array of objects. the objects contain a node id
-     * and component id.
-     */
-
-  }, {
-    key: 'getShowPreviousWorkNodeIdsAndComponentIds',
-    value: function getShowPreviousWorkNodeIdsAndComponentIds(nodeId) {
-      var nodeIdAndComponentIds = [];
-      if (nodeId != null) {
-        var nodeContent = this.getNodeContentByNodeId(nodeId);
-        if (nodeContent != null) {
-          var components = nodeContent.components;
-          if (components != null) {
-            var _iteratorNormalCompletion2 = true;
-            var _didIteratorError2 = false;
-            var _iteratorError2 = undefined;
-
-            try {
-              for (var _iterator2 = components[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-                var component = _step2.value;
-
-                if (component != null) {
-                  var showPreviousWorkNodeId = component.showPreviousWorkNodeId;
-                  var showPreviousWorkComponentId = component.showPreviousWorkComponentId;
-                  if (showPreviousWorkNodeId != null && showPreviousWorkComponentId != null) {
-                    var nodeIdAndComponentId = {};
-                    nodeIdAndComponentId.nodeId = showPreviousWorkNodeId;
-                    nodeIdAndComponentId.componentId = showPreviousWorkComponentId;
-                    nodeIdAndComponentIds.push(nodeIdAndComponentId);
-                  }
-                }
-              }
-            } catch (err) {
-              _didIteratorError2 = true;
-              _iteratorError2 = err;
-            } finally {
-              try {
-                if (!_iteratorNormalCompletion2 && _iterator2.return) {
-                  _iterator2.return();
-                }
-              } finally {
-                if (_didIteratorError2) {
-                  throw _iteratorError2;
-                }
-              }
-            }
-          }
-        }
-      }
-      return nodeIdAndComponentIds;
-    }
-
-    /**
      * Get the branch letter in the node position string if the node is in a
      * branch path
      * @param nodeId the node id we want the branch letter for
@@ -182,56 +126,56 @@ var ClassroomMonitorProjectService = function (_ProjectService) {
       this.nodeCount++;
       if (this.isGroupNode(node.id)) {
         var childIds = node.ids;
-        var _iteratorNormalCompletion3 = true;
-        var _didIteratorError3 = false;
-        var _iteratorError3 = undefined;
+        var _iteratorNormalCompletion2 = true;
+        var _didIteratorError2 = false;
+        var _iteratorError2 = undefined;
 
         try {
-          for (var _iterator3 = childIds[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-            var childId = _step3.value;
+          for (var _iterator2 = childIds[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+            var childId = _step2.value;
 
             var _child = this.getNodeById(childId);
             this.calculateNodeOrder(_child);
           }
         } catch (err) {
-          _didIteratorError3 = true;
-          _iteratorError3 = err;
+          _didIteratorError2 = true;
+          _iteratorError2 = err;
         } finally {
           try {
-            if (!_iteratorNormalCompletion3 && _iterator3.return) {
-              _iterator3.return();
+            if (!_iteratorNormalCompletion2 && _iterator2.return) {
+              _iterator2.return();
             }
           } finally {
-            if (_didIteratorError3) {
-              throw _iteratorError3;
+            if (_didIteratorError2) {
+              throw _iteratorError2;
             }
           }
         }
 
         var planningIds = node.availablePlanningNodes;
         if (planningIds) {
-          var _iteratorNormalCompletion4 = true;
-          var _didIteratorError4 = false;
-          var _iteratorError4 = undefined;
+          var _iteratorNormalCompletion3 = true;
+          var _didIteratorError3 = false;
+          var _iteratorError3 = undefined;
 
           try {
-            for (var _iterator4 = planningIds[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-              var planningId = _step4.value;
+            for (var _iterator3 = planningIds[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+              var planningId = _step3.value;
 
               var child = this.getNodeById(planningId.nodeId);
               this.calculateNodeOrder(child);
             }
           } catch (err) {
-            _didIteratorError4 = true;
-            _iteratorError4 = err;
+            _didIteratorError3 = true;
+            _iteratorError3 = err;
           } finally {
             try {
-              if (!_iteratorNormalCompletion4 && _iterator4.return) {
-                _iterator4.return();
+              if (!_iteratorNormalCompletion3 && _iterator3.return) {
+                _iterator3.return();
               }
             } finally {
-              if (_didIteratorError4) {
-                throw _iteratorError4;
+              if (_didIteratorError3) {
+                throw _iteratorError3;
               }
             }
           }
