91ca026842825a64428b67f4db8aa3d7a6c1fa19
Hiroki Terashima <honchikun@gmail.com>
2018-08-29 16:09:09 -0700

    fixed obscure unit test problem. Related to _handler.handle/subscribe/pipe.

--- src/main/webapp/site/src/app/modules/library/library.component.spec.ts
+++ src/main/webapp/site/src/app/modules/library/library.component.spec.ts
@@ -3,24 +3,53 @@ import { HttpClient, HttpHandler } from "@angular/common/http";
 import { LibraryComponent } from './library.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "../shared/shared.module";
-import { MatIconModule } from "@angular/material";
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import {
+  MatBadgeModule,
+  MatExpansionModule,
+  MatIconModule
+} from "@angular/material";
+import { Component, Input, NO_ERRORS_SCHEMA } from "@angular/core";
 import { LibraryService } from "../../services/library.service";
+import { LibraryGroup } from "./libraryGroup";
+import { LibraryProject } from "./libraryProject";
+import { Observable } from "rxjs";
+import { User } from "../../domain/user";
+
+@Component({selector: 'app-library-group-thumbs', template: ''})
+class LibraryGroupThumbsStubComponent {
+  @Input()
+  group: LibraryGroup = new LibraryGroup();
+}
+
+@Component({selector: 'app-library-project', template: ''})
+class LibraryProjectStubComponent {
+  @Input()
+  project: LibraryProject = new LibraryProject();
+}
 
 describe('LibraryComponent', () => {
   let component: LibraryComponent;
   let fixture: ComponentFixture<LibraryComponent>;
+  const libraryServiceStub = {
+    getLibraryGroups(): Observable<LibraryGroup[]> {
+      const libraryGroup: LibraryGroup[] = [];
+      return Observable.create( observer => {
+        observer.next(libraryGroup);
+        observer.complete();
+      });
+    }
 
+
+  }
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ BrowserAnimationsModule, SharedModule, MatIconModule ],
-      declarations: [ LibraryComponent ],
+      imports: [ BrowserAnimationsModule, SharedModule, MatIconModule, MatBadgeModule, MatExpansionModule ],
+      declarations: [ LibraryComponent, LibraryGroupThumbsStubComponent, LibraryProjectStubComponent ],
       providers: [
         HttpClient,
         HttpHandler,
-        LibraryService
-      ],
-      schemas: [ NO_ERRORS_SCHEMA ]
+        { provide: LibraryService, useValue: libraryServiceStub }
+      ]
     })
     .compileComponents();
   }));
