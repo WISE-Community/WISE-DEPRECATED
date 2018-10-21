b012c8463e7fcb7bb1f9dd21d0e55e50d5e7fc5e
Jonathan Lim-Breitbart <breity10@gmail.com>
2018-10-21 16:33:48 -0700

    Added unit tests to site mobile menu component

--- src/main/webapp/site/src/app/modules/mobile-menu/mobile-menu.component.spec.ts
+++ src/main/webapp/site/src/app/modules/mobile-menu/mobile-menu.component.spec.ts
@@ -1,28 +1,81 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from "@angular/router/testing";
 import { MatIconModule } from "@angular/material";
+import { Observable } from 'rxjs';
+import { NO_ERRORS_SCHEMA, Provider } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { MobileMenuComponent } from './mobile-menu.component';
+import { User } from "../../domain/user";
+import { UserService } from "../../services/user.service";
+
+export class MockUserServiceLogIn {
+  getUser(): Observable<User[]> {
+    const user: User = new User();
+    user.role = 'teacher';
+    return Observable.create(observer => {
+        observer.next(user);
+        observer.complete();
+      }
+    );
+  }
+}
+
+export class MockUserServiceLogOut {
+  getUser(): Observable<User[]> {
+    const user: User = null;
+    return Observable.create(observer => {
+        observer.next(user);
+        observer.complete();
+      }
+    );
+  }
+}
 
 describe('MobileMenuComponent', () => {
   let component: MobileMenuComponent;
   let fixture: ComponentFixture<MobileMenuComponent>;
 
-  beforeEach(async(() => {
+  function createComponent(providers: Provider[] = []) {
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule, MatIconModule ],
-      declarations: [ MobileMenuComponent ]
+      declarations: [ MobileMenuComponent ],
+      providers: [
+        { provide: UserService, useValue: new MockUserServiceLogIn() }
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(MobileMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  }
+
+  describe('', () => {
+    beforeEach(() => {
+      createComponent();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should hide account links when user logs in', () => {
+      const accountLinks = fixture.debugElement.query(By.css('#mobileMenuAccountLinks'));
+      expect(accountLinks).toBeFalsy();
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('', () => {
+    beforeEach(() => {
+      TestBed.overrideProvider(UserService, { useValue: new MockUserServiceLogOut() });
+      createComponent();
+    });
+
+    it('should show account links when user logs out', () => {
+      const accountLinks = fixture.debugElement.query(By.css('#mobileMenuAccountLinks'));
+      expect(accountLinks).toBeTruthy();
+    });
   });
 });
