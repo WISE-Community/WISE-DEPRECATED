0f6d1f2c5515d8004a2952846bba5a17adaf6b57
Hiroki Terashima <honchikun@gmail.com>
2018-12-12 15:15:24 -0800

    Changed wise.sh to use spring-boot:run

--- wise.sh
+++ wise.sh
@@ -28,7 +28,7 @@ fi
 
 if [ $1 = "package" ]; then
   ng build --configuration=production
-  mvn -Dmaven.test.skip=true package
+  mvn clean -Dmaven.test.skip=true package
   exit 0
 fi
 
@@ -63,5 +63,5 @@ if [ $1 = "dev" ]; then
   npm run watch-all-site&
 fi
 
-mvn clean tomcat7:run
+mvn spring-boot:run
 exit 0
