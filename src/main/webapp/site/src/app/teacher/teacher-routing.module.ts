afb303fc38540c1d4870a3c03a1c863439742c55
Victor Korir <vkkorir@gmail.com>
2019-01-05 13:32:11 +0300

    created all the teacher routes

--- src/main/webapp/site/src/app/teacher/teacher-routing.module.ts
+++ src/main/webapp/site/src/app/teacher/teacher-routing.module.ts
@@ -5,6 +5,11 @@ import { AuthGuard } from "./auth.guard";
 import { TeacherComponent } from "../teacher/teacher.component";
 import { TeacherHomeComponent } from "../teacher/teacher-home/teacher-home.component";
 import { EditComponent } from "./account/edit/edit.component";
+import { TeacherRunListComponent } from "../teacher/teacher-run-list/teacher-run-list.component";
+import { TeacherProjectLibraryComponent } from "../modules/library/teacher-project-library/teacher-project-library.component";
+import { OfficialLibraryComponent } from "../modules/library/official-library/official-library.component";
+import { CommunityLibraryComponent } from "../modules/library/community-library/community-library.component";
+import { PersonalLibraryComponent } from "../modules/library/personal-library/personal-library.component";
 
 const teacherRoutes: Routes = [
   {
@@ -14,7 +19,12 @@ const teacherRoutes: Routes = [
     children: [
       { path: '', component: TeacherHomeComponent },
       { path: 'profile', redirectTo: '', pathMatch: 'full' },
-      { path: 'profile/edit', component: EditComponent }
+      { path: 'profile/edit', component: EditComponent },
+      { path: 'schedule', component: TeacherRunListComponent },
+      { path: 'library', component: TeacherProjectLibraryComponent },
+      { path: 'library/tested', component: OfficialLibraryComponent },
+      { path: 'library/community', component: CommunityLibraryComponent },
+      { path: 'library/personal', component: PersonalLibraryComponent }
     ]
   }
 ];
