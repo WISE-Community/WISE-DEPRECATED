1dd014da814a8dc08d67d71a6a1f6fa5a208caf1
Hiroki Terashima <honchikun@gmail.com>
2019-02-21 16:11:53 -0800

    Added code to convert LibraryGroupJSON to LibraryGroup object. Fixes #1803.

--- src/main/webapp/site/src/app/services/library.service.spec.ts
+++ src/main/webapp/site/src/app/services/library.service.spec.ts
@@ -13,4 +13,24 @@ describe('LibraryService', () => {
   it('should be created', inject([LibraryService], (service: LibraryService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should convert LibraryGroup JSON object into LibraryGroup object', inject([LibraryService], (service: LibraryService) => {
+    const libraryGroupsJSON = [{
+      'id': '6thGrade',
+      'name': '6th grade projects',
+      'type': 'group',
+      'children': [{
+        'id': 2,
+        'dateCreated': '2019-02-21',
+        'name': 'sample project',
+        'owner': {},
+        'sharedOwners': [],
+        'projectThumb': '17/assets/projectThumb.png',
+        'type': 'project',
+        'wiseVersion': 5
+      }]
+    }];
+    const convertedLibraryGroups = service.convertLibraryGroups(libraryGroupsJSON);
+    expect(convertedLibraryGroups[0].children[0].constructor.name).toEqual('LibraryProject');
+  }));
 });
