9117f408b67385bcb7932f90a89a84b8857442a5
Victor Korir <vkkorir@gmail.com>
2019-02-22 16:04:22 -0800

    completed alert no user found warning

--- src/main/webapp/site/src/app/student/team-sign-in-dialog/team-sign-in-dialog.component.ts
+++ src/main/webapp/site/src/app/student/team-sign-in-dialog/team-sign-in-dialog.component.ts
@@ -134,6 +134,8 @@ export class TeamSignInDialogComponent implements OnInit {
                     alert(this.i18n('{{firstName}} {{lastName}} is already on another team.', {firstName: response.firstName, lastName: response.lastName}));
                   }
                 });
+            } else {
+              alert(this.i18n('No WISE user with this Google ID found'));
             }
           });
         }
