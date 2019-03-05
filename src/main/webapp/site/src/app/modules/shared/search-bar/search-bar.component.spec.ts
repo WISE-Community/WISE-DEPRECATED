d60619ff3cd2c8fec92bd4ce9958b1b3cfc16280
Jonathan Lim-Breitbart <breity10@gmail.com>
2019-03-05 12:33:23 -0800

    Added some translations in code

--- src/main/webapp/site/src/app/modules/shared/search-bar/search-bar.component.spec.ts
+++ src/main/webapp/site/src/app/modules/shared/search-bar/search-bar.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchBarComponent } from './search-bar.component';
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, TRANSLATIONS_FORMAT, TRANSLATIONS, LOCALE_ID } from "@angular/core";
+import { I18n } from '@ngx-translate/i18n-polyfill';
+import { translationsFactory } from "../../../app.module";
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
@@ -9,7 +11,15 @@ describe('SearchBarComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SearchBarComponent ],
-      imports: [],
+      imports: [ ],
+      providers: [
+        { provide: TRANSLATIONS_FORMAT, useValue: "xlf" },
+        {
+          provide: TRANSLATIONS,
+          useFactory: translationsFactory,
+          deps: [LOCALE_ID]
+        },
+        I18n ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
--- src/main/webapp/site/src/app/modules/shared/select-menu/select-menu.component.spec.ts
+++ src/main/webapp/site/src/app/modules/shared/select-menu/select-menu.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectMenuComponent } from './select-menu.component';
-import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NO_ERRORS_SCHEMA, TRANSLATIONS_FORMAT, TRANSLATIONS, LOCALE_ID } from "@angular/core";
+import { I18n } from '@ngx-translate/i18n-polyfill';
+import { translationsFactory } from "../../../app.module";
 
 describe('SelectMenuComponent', () => {
   let component: SelectMenuComponent;
@@ -9,7 +11,16 @@ describe('SelectMenuComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ SelectMenuComponent ],
-      imports: [],
+      imports: [ ],
+      providers: [
+        { provide: TRANSLATIONS_FORMAT, useValue: "xlf" },
+        {
+          provide: TRANSLATIONS,
+          useFactory: translationsFactory,
+          deps: [LOCALE_ID]
+        },
+        I18n
+      ],
       schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
