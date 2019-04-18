0b732cf4570dfe8507ba4df52564da8c892fe418
geoffreykwan <geoffreykwan@gmail.com>
2019-04-18 15:35:19 -0400

    Fixed a problem with not being able to connect to websockets because of allowed origins settings. #1834

--- src/main/java/org/wise/portal/spring/impl/WebSocketConfig.java
+++ src/main/java/org/wise/portal/spring/impl/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/websocket").withSockJS();
+    registry.addEndpoint("/websocket").setAllowedOrigins("*").withSockJS();
   }
 
   @Override
