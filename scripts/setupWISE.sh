97522be3227cc1f0606b5ea8d4c5d2c120ac664c
geoffreykwan <geoffreykwan@gmail.com>
2019-08-23 18:52:28 -0400

    Added deployment group name check for qa or prod to the deployment scripts.

---
+++ scripts/setupWISE.sh
@@ -0,0 +1,10 @@
+export SETUP_DIR=/home/ubuntu/setup
+
+cd $SETUP_DIR
+if [ "$DEPLOYMENT_GROUP_NAME" == "qa-deployment-group" ]
+then
+    ./setupServer.sh qa
+elif [ "$DEPLOYMENT_GROUP_NAME" == "prod-deployment-group" ]
+then
+    ./setupServer.sh prod
+fi
