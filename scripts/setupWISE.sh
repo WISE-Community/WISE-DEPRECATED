2c81712be34cebef275ac8141d1e151ad9e946dd
geoffreykwan <geoffreykwan@gmail.com>
2019-08-23 19:15:34 -0400

    Removed some quotes that I missed in the previous commit.

--- scripts/setupWISE.sh
+++ scripts/setupWISE.sh
@@ -3,7 +3,7 @@
 export SETUP_DIR=/home/ubuntu/setup
 
 cd $SETUP_DIR
-if [ "$DEPLOYMENT_GROUP_NAME" == "qa-deployment-group" ];
+if [ $DEPLOYMENT_GROUP_NAME == "qa-deployment-group" ];
 then
     ./setupServer.sh qa
 elif [ $DEPLOYMENT_GROUP_NAME == "prod-deployment-group" ];
