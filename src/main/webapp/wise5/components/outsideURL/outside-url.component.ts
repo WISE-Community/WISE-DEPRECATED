59ce2688a536c07aa5042fc4dedc67a4b0c5f71c
geoffreykwan <geoffreykwan@gmail.com>
2019-11-20 14:18:51 -0500

    Started working on converting OutsideURL component from AngularJS to Angular. #2128

---
+++ src/main/webapp/wise5/components/outsideURL/outside-url.component.ts
@@ -0,0 +1,90 @@
+'use strict';
+
+import * as angular from 'angular';
+import { Component } from '@angular/core';
+import { Activity } from "../activity";
+import { downgradeComponent } from '@angular/upgrade/static';
+declare var angular: angular.IAngularStatic;
+
+@Component({
+  selector: 'outside-url',
+  template: './index.html'
+})
+export class OutsideURL extends Activity {
+
+  $q;
+  $sce;
+  OutsideURLService;
+  url;
+  width;
+  height;
+
+  constructor($filter,
+      $mdDialog,
+      $q,
+      $rootScope,
+      $sce,
+      $scope,
+      AnnotationService,
+      ConfigService,
+      NodeService,
+      NotebookService,
+      OutsideURLService,
+      ProjectService,
+      StudentAssetService,
+      StudentDataService,
+      UtilService) {
+    super($filter, $mdDialog, $rootScope, $scope,
+        AnnotationService, ConfigService, NodeService,
+        NotebookService, ProjectService, StudentAssetService,
+        StudentDataService, UtilService);
+    this.$q = $q;
+    this.$sce = $sce;
+    this.OutsideURLService = OutsideURLService;
+
+    // the url to the web page to display
+    this.url = null;
+
+    if (this.componentContent != null) {
+      // set the url
+      this.setURL(this.componentContent.url);
+    }
+
+    this.setWidthAndHeight(this.componentContent.width, this.componentContent.height);
+
+    /**
+     * Get the component state from this component. The parent node will
+     * call this function to obtain the component state when it needs to
+     * save student data.
+     * @return a promise of a component state containing the student data
+     */
+    this.$scope.getComponentState = function() {
+      var deferred = this.$q.defer();
+
+      /*
+       * the student does not have any unsaved changes in this component
+       * so we don't need to save a component state for this component.
+       * we will immediately resolve the promise here.
+       */
+      deferred.resolve();
+
+      return deferred.promise;
+    }.bind(this);
+
+    this.$rootScope.$broadcast('doneRenderingComponent', { nodeId: this.nodeId, componentId: this.componentId });
+  }
+
+  setWidthAndHeight(width, height) {
+    this.width = width ? width + 'px' : 'none';
+    this.height = height ? height + 'px' : '600px';
+  }
+
+  setURL(url) {
+    if (url == null || url === '') {
+      this.url = ' ';
+    } else {
+      this.url = this.$sce.trustAsResourceUrl(url);
+    }
+  };
+}
+
---
+++ src/main/webapp/wise5/components/outsideURL/outside-url.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { OutsideURL } from "./outside-url.component";
+
+@NgModule({
+  imports: [],
+  declarations: [
+    OutsideURL
+  ],
+  entryComponents: [],
+  providers: [],
+  exports: []
+})
+export class OutsideURLModule { }
