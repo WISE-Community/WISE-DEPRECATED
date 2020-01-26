6fe5eddca04a2cdf6369cfa65be4bb01d1687443
Anthony Perritano <aperritano@gmail.com>
2020-01-26 14:59:55 -0600

    transactionManager added for JPA repo config

--- src/main/java/org/wise/portal/spring/impl/RepositoryConfig.java
+++ src/main/java/org/wise/portal/spring/impl/RepositoryConfig.java
@@ -1,22 +1,24 @@
 package org.wise.portal.spring.impl;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 
 @Configuration
 @EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = {"org.wise.portal.score.repository"})
 @EntityScan(basePackages = {"org.wise.portal"})
 @EnableTransactionManagement
-public class RepositoryConfiguration {
+public class RepositoryConfig {
 
   @PersistenceContext
   private EntityManager entityManager;
@@ -24,6 +26,13 @@ public class RepositoryConfiguration {
   @Bean
   public EntityManagerFactory entityManagerFactory() {
     return entityManager.getEntityManagerFactory();
+  }
+
+  @Bean(name = "transactionManager")
+  public PlatformTransactionManager transactionManager() {
+    JpaTransactionManager txManager = new JpaTransactionManager();
+    txManager.setEntityManagerFactory(entityManagerFactory());
+    return txManager;
   }
 }
 
