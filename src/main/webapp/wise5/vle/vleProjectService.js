58a42acbe2cad60f1990455a4941a5e007e65e0e
geoffreykwan <geoffreykwan@gmail.com>
2020-02-03 18:24:28 -0500

    Cleaned up vleProjectService.js and wrote some tests.

--- src/main/webapp/wise5/vle/vleProjectService.js
+++ src/main/webapp/wise5/vle/vleProjectService.js
@@ -7,7 +7,6 @@ class VLEProjectService extends ProjectService {
   }
 
   /**
-   * Check if a component is a connected component
    * @param nodeId the node id of the component
    * @param componentId the component that is listening for connected changes
    * @param connectedComponentId the component that is broadcasting connected changes
@@ -15,59 +14,29 @@ class VLEProjectService extends ProjectService {
    */
   isConnectedComponent(nodeId, componentId, connectedComponentId) {
     const component = this.getComponentByNodeIdAndComponentId(nodeId, componentId);
-    if (component != null) {
-      const connectedComponents = component.connectedComponents;
-      if (connectedComponents != null) {
-        for (let connectedComponent of connectedComponents) {
-          if (connectedComponent != null) {
-            /*
-             * check if the connected component id matches the one
-             * we are looking for. connectedComponent.id is the old
-             * field we used to store the component id in so we will
-             * look for that field for the sake of backwards
-             * compatibility. connectedComponent.componentId is the
-             * new field we store the component id in.
-             */
-            if (connectedComponentId === connectedComponent.id ||
-              connectedComponentId === connectedComponent.componentId) {
-              return true;
-            }
-          }
-        }
+    for (const connectedComponent of component.connectedComponents) {
+      if (this.isMatchingConnectedComponent(connectedComponent, connectedComponentId)) {
+        return true;
       }
     }
     return false;
   }
 
+  isMatchingConnectedComponent(connectedComponent, id) {
+    return connectedComponent.id === id || connectedComponent.componentId === id;
+  }
+
   /**
-   * Get a connected component params
    * @param componentId the connected component id
    * @returns the params for the connected component
    */
   getConnectedComponentParams(componentContent, componentId) {
-    let connectedComponentParams = null;
-    if (componentContent != null && componentId != null) {
-      const connectedComponents = componentContent.connectedComponents;
-      if (connectedComponents != null) {
-        for (let connectedComponent of connectedComponents) {
-          if (connectedComponent != null) {
-            /*
-             * check if the connected component id matches the one
-             * we are looking for. connectedComponent.id is the old
-             * field we used to store the component id in so we will
-             * look for that field for the sake of backwards
-             * compatibility. connectedComponent.componentId is the
-             * new field we store the component id in.
-             */
-            if (componentId === connectedComponent.id ||
-              componentId === connectedComponent.componentId) {
-              connectedComponentParams = connectedComponent;
-            }
-          }
-        }
+    for (const connectedComponent of componentContent.connectedComponents) {
+      if (this.isMatchingConnectedComponent(connectedComponent, componentId)) {
+        return connectedComponent;
       }
     }
-    return connectedComponentParams;
+    return null;
   }
 
   /**
@@ -76,135 +45,88 @@ class VLEProjectService extends ProjectService {
    * @returns {boolean} whether we need to display the annotation to the student
    */
   displayAnnotation(annotation) {
-    const component = this.getComponentByNodeIdAndComponentId(annotation.nodeId, annotation.componentId);
-    if (component != null) {
-      const componentService = this.$injector.get(component.type + 'Service');
-      return componentService.displayAnnotation(component, annotation);
-    }
-    return true;
+    const component =
+        this.getComponentByNodeIdAndComponentId(annotation.nodeId, annotation.componentId);
+    const componentService = this.$injector.get(component.type + 'Service');
+    return componentService.displayAnnotation(component, annotation);
   }
 
   /**
    * Get the global annotation properties for the specified component and score, if exists.
    * @param component the component content
-   * @param previousScore the previousScore we want the annotation properties for, can be null, which means we just want to look at
-   * the currentScore
+   * @param previousScore the previousScore we want the annotation properties for, can be null,
+   * which means we just want to look at the currentScore
    * @param currentScore the currentScore we want the annotation properties for
    * @returns the annotation properties for the given score
    */
   getGlobalAnnotationGroupByScore(component, previousScore, currentScore) {
-    let annotationGroup = null;
-    if (component.globalAnnotationSettings != null &&
-      component.globalAnnotationSettings.globalAnnotationGroups != null) {
-      let globalAnnotationGroups = component.globalAnnotationSettings.globalAnnotationGroups;
-      for (let globalAnnotationGroup of globalAnnotationGroups) {
-        if (globalAnnotationGroup.enableCriteria != null &&
-          globalAnnotationGroup.enableCriteria.scoreSequence != null) {
-          let scoreSequence = globalAnnotationGroup.enableCriteria.scoreSequence;
-          if (scoreSequence != null) {
-            /*
-             * get the expected previous score and current score
-             * that will satisfy the rule
-             */
-            let previousScoreMatch = scoreSequence[0];
-            let currentScoreMatch = scoreSequence[1];
-
-            if (previousScore == null) {
-              // just matching on the current score
-              if (previousScoreMatch == "" &&
-                currentScore.toString().match("[" + currentScoreMatch + "]")) {
-                // found a match
-                annotationGroup = globalAnnotationGroup;
-                break;
-              }
-            } else {
-              if (previousScore.toString().match("[" + previousScoreMatch + "]") &&
-                currentScore.toString().match("[" + currentScoreMatch + "]")) {
-                /*
-                 * the previous score and current score match the
-                 * expected scores so we have found the rule we want
-                 */
-                annotationGroup = globalAnnotationGroup;
-                break;
-              }
-            }
-          }
-        }
+    for (const globalAnnotationGroup of component.globalAnnotationSettings.globalAnnotationGroups) {
+      const scoreSequence = globalAnnotationGroup.enableCriteria.scoreSequence;
+      if (this.isScoreSequenceMatch(scoreSequence, previousScore, currentScore)) {
+        return globalAnnotationGroup;
       }
     }
-    return annotationGroup;
+    return null;
+  }
+
+  isScoreSequenceMatch(scoreSequence, previousScore, currentScore) {
+    const previousScoreMatch = this.getExpectedPreviousScore(scoreSequence);
+    const currentScoreMatch = this.getExpectedCurrentScore(scoreSequence);
+    if (previousScore == null) {
+      if (previousScoreMatch === '' && this.isScoreMatch(currentScore, currentScoreMatch)) {
+        return true;
+      }
+    } else if (this.isScoreMatch(previousScore, previousScoreMatch) &&
+        this.isScoreMatch(currentScore, currentScoreMatch)) {
+      return true;
+    }
+    return false;
+  }
+
+  getExpectedPreviousScore(scoreSequence) {
+    return scoreSequence[0];
+  }
+
+  getExpectedCurrentScore(scoreSequence) {
+    return scoreSequence[1];
+  }
+
+  isScoreMatch(score, expectedScore) {
+    return score.toString().match(`[${expectedScore}]`) != null;
   }
 
   /**
-   * Get the notification for the given score, if exists.
    * @param component the component content
-   * @param previousScore the previousScore we want notification for, can be null, which means we just want to look at
-   * the currentScore
+   * @param previousScore the previousScore we want notification for, can be null, which means we 
+   * just want to look at the currentScore
    * @param currentScore the currentScore we want notification for
    * @returns the notification for the given score
    */
   getNotificationByScore(component, previousScore, currentScore) {
-    let notificationResult = null;
-    if (component.notificationSettings != null &&
-      component.notificationSettings.notifications != null) {
-      let notifications = component.notificationSettings.notifications;
-      for (let notification of notifications) {
-        if (notification.enableCriteria != null &&
-          notification.enableCriteria.scoreSequence != null) {
-          let scoreSequence = notification.enableCriteria.scoreSequence;
-          if (scoreSequence != null) {
-            /*
-             * get the expected previous score and current score
-             * that will satisfy the rule
-             */
-            let previousScoreMatch = scoreSequence[0];
-            let currentScoreMatch = scoreSequence[1];
-
-            if (previousScore == null) {
-              // just matching on the current score
-              if (previousScoreMatch == "" &&
-                currentScore.toString().match("[" + currentScoreMatch + "]")) {
-                notificationResult = notification;
-                break;
-              }
-            } else {
-              if (previousScore.toString().match("[" + previousScoreMatch + "]") &&
-                currentScore.toString().match("[" + currentScoreMatch + "]")) {
-                /*
-                 * the previous score and current score match the
-                 * expected scores so we have found the rule we want
-                 */
-                notificationResult = notification;
-                break;
-              }
-            }
-          }
-        }
+    for (const notification of component.notificationSettings.notifications) {
+      const scoreSequence = notification.enableCriteria.scoreSequence;
+      if (this.isScoreSequenceMatch(scoreSequence, previousScore, currentScore)) {
+        return notification;
       }
     }
-    return notificationResult;
+    return null;
   }
 
-  /**
-   * Retrieve the script with the provided script filename
-   * @param scriptFilename
-   */
   retrieveScript(scriptFilename) {
-    let assetDirectoryPath = this.ConfigService.getProjectAssetsDirectoryPath();
-    let scriptPath = assetDirectoryPath + "/" + scriptFilename;
+    const assetDirectoryPath = this.ConfigService.getProjectAssetsDirectoryPath();
+    const scriptPath = assetDirectoryPath + "/" + scriptFilename;
     return this.$http.get(scriptPath).then((result) => {
       return result.data;
     });
-  };
+  }
 
   /**
-   * Registers an additionalProcessingFunction for the specified node and component
    * @param nodeId the node id
    * @param componentId the component id
    * @param additionalProcessingFunction the function to register for the node and component.
    */
   addAdditionalProcessingFunction(nodeId, componentId, additionalProcessingFunction) {
-    let key = nodeId + "_" + componentId;
+    const key = nodeId + "_" + componentId;
     if (this.additionalProcessingFunctionsMap[key] == null) {
       this.additionalProcessingFunctionsMap[key] = [];
     }
