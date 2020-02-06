3a5f1babd8d6c269e94a93622cca247ee4bc8981
Jonathan Lim-Breitbart <breity10@gmail.com>
2020-02-06 14:29:10 -0800

    Fixed node icon not updating when navigating to a different node. Fixed node icon alignment. #2200

--- src/main/webapp/wise5/classroomMonitor/classroomMonitorComponents/nodeGrading/stepTools/stepTools.js
+++ src/main/webapp/wise5/classroomMonitor/classroomMonitorComponents/nodeGrading/stepTools/stepTools.js
@@ -97,7 +97,7 @@ const StepTools = {
                 <md-icon> {{ ::$ctrl.icons.prev }} </md-icon>
                 <md-tooltip md-direction="bottom">{{ ::'PREVIOUS_STEP' | translate }}</md-tooltip>
             </md-button>
-            <node-icon node-id="::$ctrl.nodeId" size="18"></node-icon>&nbsp;
+            <node-icon node-id="$ctrl.nodeId" size="18"></node-icon>&nbsp;
             <md-select id="stepSelectMenu" md-theme="default"
                        class="md-button md-no-underline toolbar__select toolbar__select--fixedwidth"
                        md-container-class="stepSelectMenuContainer"
--- src/main/webapp/wise5/classroomMonitor/classroomMonitorComponents/shared/nodeIcon/nodeIcon.js
+++ src/main/webapp/wise5/classroomMonitor/classroomMonitorComponents/shared/nodeIcon/nodeIcon.js
@@ -35,9 +35,9 @@ const NodeIcon = {
   },
   controller: NodeIconController,
   template:
-    `<img ng-if="::$ctrl.isImage()" ng-animate-ref="{{ ::$ctrl.nodeId }}" class="{{ ::$ctrl.isGroup ? 'avatar--square ' : '' }}{{ ::$ctrl.customClass }} {{ ::$ctrl.sizeClass }} avatar" ng-src="{{ ::$ctrl.icon.imgSrc }}" alt="{{ ::$ctrl.icon.imgAlt }}" />
-      <div ng-if="::$ctrl.isFont()" ng-animate-ref="{{ ::$ctrl.nodeId }}" style="background-color: {{ ::$ctrl.icon.color }};" class="{{ ::$ctrl.isGroup ? 'avatar--square ' : '' }}{{ ::$ctrl.customClass }} {{ ::$ctrl.sizeClass }} avatar avatar--icon">
-        <md-icon class="{{ ::$ctrl.sizeClass }} {{ ::$ctrl.icon.fontSet }} md-light node-icon">{{ ::$ctrl.icon.fontName }}</md-icon>
+    `<img ng-if="$ctrl.isImage()" ng-animate-ref="{{ $ctrl.nodeId }}" class="{{ $ctrl.isGroup ? 'avatar--square ' : '' }}{{ $ctrl.customClass }} {{ $ctrl.sizeClass }} avatar" ng-src="{{ $ctrl.icon.imgSrc }}" alt="{{ $ctrl.icon.imgAlt }}" />
+      <div ng-if="$ctrl.isFont()" ng-animate-ref="{{ $ctrl.nodeId }}" style="background-color: {{ $ctrl.icon.color }};" class="{{ $ctrl.isGroup ? 'avatar--square ' : '' }}{{ $ctrl.customClass }} {{ $ctrl.sizeClass }} avatar avatar--icon">
+        <md-icon class="{{ $ctrl.sizeClass }} {{ $ctrl.icon.fontSet }} md-light node-icon">{{ $ctrl.icon.fontName }}</md-icon>
       </div>`
 };
 
