9f407a4bd6ba4e0da0a82d0c1094be5d714c62cd
Victor Korir <vkkorir@gmail.com>
2020-03-15 22:33:43 -0700

    added ckeditor library to add news item dialog component

--- src/main/webapp/site/src/app/news/news-item-dialog/news-item-dialog.component.ts
+++ src/main/webapp/site/src/app/news/news-item-dialog/news-item-dialog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-news-item-dialog',
@@ -9,6 +11,12 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 export class NewsItemDialogComponent implements OnInit {
 
   isEditMode: boolean;
+  public Editor = ClassicEditor;
+
+  formGroup: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    content: new FormControl('')
+  });
 
   constructor(private dialogRef: MatDialogRef<NewsItemDialogComponent>,
               @Inject(MAT_DIALOG_DATA) private data: any) {
--- src/main/webapp/site/src/app/news/news.module.ts
+++ src/main/webapp/site/src/app/news/news.module.ts
@@ -4,23 +4,25 @@ import { TimelineModule } from "../modules/timeline/timeline.module";
 import { NewsComponent } from "./news.component";
 import { MomentModule } from 'ngx-moment';
 import { NewsRoutingModule } from "./news-routing.module";
-import { MatButtonModule, MatCardModule, MatIconModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from '../modules/shared/shared.module';
 import { NewsItemDialogComponent } from './news-item-dialog/news-item-dialog.component';
+import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
     MomentModule,
     NewsRoutingModule,
     TimelineModule,
     SharedModule,
-    MatButtonModule
+    CKEditorModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule
   ],
   declarations: [
     NewsComponent,
