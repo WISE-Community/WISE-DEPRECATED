b1bf1b99f133afe65abc69f103791baa91d5eeaa
Jonathan Lim-Breitbart <breity10@gmail.com>
2020-04-13 15:36:35 -0700

    Added tests for the MilestoneService. #2299

--- src/main/webapp/wise5/test-unit/services/milestoneService.spec.js
+++ src/main/webapp/wise5/test-unit/services/milestoneService.spec.js
@@ -1,5 +1,21 @@
 import classroomMonitorModule from '../../classroomMonitor/classroomMonitor';
 let MilestoneService, ProjectService;
+const satisfyCriterionNotEqualTo = {
+  percentThreshold: 50,
+  targetVariable: 'ki',
+  componentId: 'xfns1g7pga',
+  function: 'percentOfScoresNotEqualTo',
+  id: 'template1SatisfyCriteria0',
+  type: 'autoScore',
+  nodeId: 'node1',
+  value: 3
+};
+const aggregateAutoScoresSample = {
+  xfns1g7pga: {
+    ki: {counts: {1: 2, 2: 0, 3: 1, 4: 0, 5: 0}, scoreCount: 3}
+  }
+};
+const possibleScoresKi = [1,2,3,4,5];
 
 describe('UtilService', () => {
   beforeEach(angular.mock.module(classroomMonitorModule.name));
@@ -9,6 +25,12 @@ describe('UtilService', () => {
   }));
 
   getProjectMilestones();
+  isPercentOfScoresNotEqualTo();
+  getNotEqualToSum();
+  getAggregateData();
+  getPossibleScores();
+  isPercentThresholdSatisfied();
+  getSatisfyCriteriaReferencedComponents();
 });
 
 function getProjectMilestones() {
@@ -33,6 +55,122 @@ function getProjectMilestones() {
       spyOn(ProjectService, 'getAchievements').and.returnValue(achievements);
       const milestones = MilestoneService.getProjectMilestones();
       expect(milestones.length).toEqual(2);
+    });
+  });
+}
+
+function isPercentOfScoresNotEqualTo() {
+  describe('isPercentOfScoresNotEqualTo()', () => {
+    it('should return true when percent of scores equal to value are less than threshold', () => {
+      const result = 
+          MilestoneService.isPercentOfScoresNotEqualTo(satisfyCriterionNotEqualTo, aggregateAutoScoresSample);
+      expect(result)
+          .toBeTruthy();
+    });
+    it('should return true when percent of scores equal to value meet threshold', () => {
+      const aggregateAutoScores = angular.copy(aggregateAutoScoresSample)
+      aggregateAutoScores.xfns1g7pga.ki.counts = {1: 1, 2: 0, 3: 2, 4: 0, 5: 0};
+      const result = 
+          MilestoneService.isPercentOfScoresNotEqualTo(satisfyCriterionNotEqualTo, aggregateAutoScores);
+      expect(result).toBeFalsy();
+    });
+  });
+}
+
+function getNotEqualToSum() {
+  describe('getNotEqualToSum()', () => {
+    const aggregateData = {
+      counts: {1: 2, 2: 0, 3: 1, 4: 0, 5: 0}, 
+      scoreCount: 3
+    };
+    it('should return the sum of scores not equal to value', () => {
+      const result = 
+          MilestoneService.getNotEqualToSum(satisfyCriterionNotEqualTo, aggregateData, possibleScoresKi);
+      expect(result).toBe(2);
+    });
+  });
+}
+
+function getAggregateData() {
+  describe('getAggregateData()', () => {
+    it('should return the aggregate data', () => {
+      const result = MilestoneService.getAggregateData(satisfyCriterionNotEqualTo, aggregateAutoScoresSample);
+      expect(result).toEqual({
+        counts: {1: 2, 2: 0, 3: 1, 4: 0, 5: 0}, 
+        scoreCount: 3
+      });
+    });
+  });
+}
+
+function getPossibleScores() {
+  describe('getPossibleScores()', () => {
+    const aggregateData = {
+      counts: {2: 2, 1: 0, 3: 1, 4: 0, 5: 0}
+    };
+    it('should return the possible scores', () => {
+      expect(MilestoneService.getPossibleScores(aggregateData)).toEqual([
+        1, 2, 3, 4, 5
+      ]);
+    });
+  });
+}
+
+function isPercentThresholdSatisfied() {
+  describe('isPercentThresholdSatisfied()', () => {
+    it('should return true when percent threshold is satisfied', () => {
+      const aggregateAutoScores = {
+        xfns1g7pga: {
+          ki: {counts: {1: 1, 2: 0, 3: 2, 4: 0, 5: 0}, scoreCount: 3}
+        }
+      };
+      const aggregateData = 
+          MilestoneService.getAggregateData(satisfyCriterionNotEqualTo, aggregateAutoScores);
+      const sum = 
+          MilestoneService.getEqualToSum(satisfyCriterionNotEqualTo, aggregateData, possibleScoresKi);
+      const result = 
+          MilestoneService.isPercentThresholdSatisfied(satisfyCriterionNotEqualTo, aggregateData, sum);
+      expect(result).toBeTruthy();
+    });
+    it('should return false when percent threshold is not satisfied', () => {
+      const aggregateData = 
+          MilestoneService.getAggregateData(satisfyCriterionNotEqualTo, aggregateAutoScoresSample);
+      const sum = 
+          MilestoneService.getEqualToSum(satisfyCriterionNotEqualTo, aggregateData, possibleScoresKi);
+      const result = 
+          MilestoneService.isPercentThresholdSatisfied(satisfyCriterionNotEqualTo, aggregateData, sum);
+      expect(result).toBeFalsy();
+    });
+  });
+}
+
+function getSatisfyCriteriaReferencedComponents() {
+  describe('getSatisfyCriteriaReferencedComponents()', () => {
+    it('should return referenced components', () => {
+      const satisfyCriterion = angular.copy(satisfyCriterionNotEqualTo);
+      satisfyCriterion.nodeId = 'node2';
+      const projectAchievement = {
+        report: {
+          templates: [
+            {
+              satisfyCriteria: [
+                satisfyCriterionNotEqualTo,
+                satisfyCriterion
+              ]
+            }
+          ]
+        }
+      };
+      expect(MilestoneService.getSatisfyCriteriaReferencedComponents(projectAchievement)).toEqual({
+        'node1_xfns1g7pga': {
+          nodeId: 'node1',
+          componentId: 'xfns1g7pga'
+        },
+        'node2_xfns1g7pga': {
+          nodeId: 'node2',
+          componentId: 'xfns1g7pga'
+        }
+      });
     });
   });
 }
