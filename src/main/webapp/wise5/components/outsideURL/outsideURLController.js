839995ce2d6192534048b0e15b1d0345c0599f10
geoffreykwan <geoffreykwan@gmail.com>
2020-04-29 12:46:38 -0400

    Fixed a problem with saving in a step containing an outside url component.

--- src/main/webapp/wise5/components/outsideURL/outsideURLController.js
+++ src/main/webapp/wise5/components/outsideURL/outsideURLController.js
@@ -1,27 +1,39 @@
 'use strict';
 
-import ComponentController from "../componentController";
+import ComponentController from '../componentController';
 
 class OutsideURLController extends ComponentController {
-  constructor($filter,
+  constructor(
+    $filter,
+    $mdDialog,
+    $q,
+    $rootScope,
+    $sce,
+    $scope,
+    AnnotationService,
+    ConfigService,
+    NodeService,
+    NotebookService,
+    OutsideURLService,
+    ProjectService,
+    StudentAssetService,
+    StudentDataService,
+    UtilService
+  ) {
+    super(
+      $filter,
       $mdDialog,
-      $q,
       $rootScope,
-      $sce,
       $scope,
       AnnotationService,
       ConfigService,
       NodeService,
       NotebookService,
-      OutsideURLService,
       ProjectService,
       StudentAssetService,
       StudentDataService,
-      UtilService) {
-    super($filter, $mdDialog, $rootScope, $scope,
-        AnnotationService, ConfigService, NodeService,
-        NotebookService, ProjectService, StudentAssetService,
-        StudentDataService, UtilService);
+      UtilService
+    );
     this.$q = $q;
     this.$sce = $sce;
     this.OutsideURLService = OutsideURLService;
@@ -36,25 +48,10 @@ class OutsideURLController extends ComponentController {
 
     this.setWidthAndHeight(this.componentContent.width, this.componentContent.height);
 
-    /**
-     * Get the component state from this component. The parent node will
-     * call this function to obtain the component state when it needs to
-     * save student data.
-     * @return a promise of a component state containing the student data
-     */
-    this.$scope.getComponentState = function() {
-      const deferred = this.$q.defer();
-
-      /*
-       * the student does not have any unsaved changes in this component
-       * so we don't need to save a component state for this component.
-       * we will immediately resolve the promise here.
-       */
-      deferred.resolve();
-      return deferred.promise;
-    }.bind(this);
-
-    this.$rootScope.$broadcast('doneRenderingComponent', { nodeId: this.nodeId, componentId: this.componentId });
+    this.$rootScope.$broadcast('doneRenderingComponent', {
+      nodeId: this.nodeId,
+      componentId: this.componentId
+    });
   }
 
   setWidthAndHeight(width, height) {
