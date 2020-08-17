d933df1fe6397f0042c168b295f88e72648a42dc
Hiroki Terashima <honchikun@gmail.com>
2020-08-17 15:23:25 -0700

    Removed unused code from navigation controller.

--- src/main/webapp/wise5/vle/navigation/navigationController.ts
+++ src/main/webapp/wise5/vle/navigation/navigationController.ts
@@ -2,19 +2,10 @@ import { VLEProjectService } from '../vleProjectService';
 
 class NavigationController {
   rootNode: any;
-  static $inject = ['$transitions', 'ProjectService'];
+  static $inject = ['ProjectService'];
 
-  constructor(
-    $transitions,
-    private ProjectService: VLEProjectService
-  ) {
+  constructor(private ProjectService: VLEProjectService) {
     this.rootNode = this.ProjectService.rootNode;
-    $transitions.onSuccess({}, $transition => {
-      const toNodeId = $transition.params('to').nodeId;
-      if ($transition.name === 'root.vle' && this.ProjectService.isApplicationNode(toNodeId)) {
-        document.getElementById('content').scrollTop = 0;
-      }
-    });
   }
 }
 
