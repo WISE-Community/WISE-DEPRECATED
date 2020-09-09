dbaaf209d18c582ebdbf47ebe879e58c16c2c70b
Hiroki Terashima <honchikun@gmail.com>
2020-09-08 19:56:09 -0700

    Extracted add component feature from NodeAuthoringController into its own route. Split add component UX to two parts: one to select component type and one to choose where it goes. #2602

---
+++ src/main/webapp/wise5/authoringTool/addComponent/addComponentModule.ts
@@ -0,0 +1,26 @@
+import * as angular from 'angular';
+import { ChooseNewComponent } from './choose-new-component.component';
+import { ChooseNewComponentLocation } from './choose-new-component-location.component';
+
+export default angular.module('addComponentModule', ['ui.router'])
+  .component('chooseNewComponent', ChooseNewComponent)
+  .component('chooseNewComponentLocation', ChooseNewComponentLocation)
+  .config(['$stateProvider', $stateProvider => {
+    $stateProvider
+      .state('root.at.project.node.add-component', {
+        url: '/add-component',
+        abstract: true,
+        resolve: {}
+      })
+      .state('root.at.project.node.add-component.choose-component', {
+        url: '/choose-component',
+        component: 'chooseNewComponent'
+      })
+      .state('root.at.project.node.add-component.choose-location', {
+        url: '/choose-location',
+        component: 'chooseNewComponentLocation',
+        params: {
+          componentType: ''
+        }
+      });
+  }]);
