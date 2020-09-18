0c0a3fecc983d1779d59774ed0ecd5780c8c52b3
Geoffrey Kwan <geoffreykwan@gmail.com>
2020-09-18 10:57:38 -0400

    Refactored createWISELinkLinkElement and createWISELinkButtonElement functions. #2626

--- src/main/webapp/wise5/components/html/htmlAuthoringController.ts
+++ src/main/webapp/wise5/components/html/htmlAuthoringController.ts
@@ -147,16 +147,44 @@ class HTMLAuthoringController extends HTMLController {
   createWISELink({ nodeId, componentId, wiseLinkNodeId, wiseLinkComponentId, wiseLinkType,
       wiseLinkText, target }) {
     if (nodeId === this.nodeId && componentId === this.componentId && target === 'prompt') {
-      if (wiseLinkType === 'link') {
-        this.injectWISELinkToPrompt(
-          this.createWISELinkLinkElement(wiseLinkNodeId, wiseLinkComponentId, wiseLinkText)
-        );
-      } else {
-        this.injectWISELinkToPrompt(
-          this.createWISELinkButtonElement(wiseLinkNodeId, wiseLinkComponentId, wiseLinkText)
-        );
-      }
+      this.injectWISELinkToPrompt(
+        this.createWISELinkElement(wiseLinkType, wiseLinkNodeId, wiseLinkComponentId, wiseLinkText)
+      );
     }
+  }
+
+  injectWISELinkToPrompt(wiseLinkElement) {
+    const summernoteId = 'summernotePrompt_' + this.nodeId + '_' + this.componentId;
+    angular.element(document.querySelector(`#${summernoteId}`)).summernote('editor.restoreRange');
+    angular.element(document.querySelector(`#${summernoteId}`)).summernote('editor.focus');
+    angular
+      .element(document.querySelector(`#${summernoteId}`))
+      .summernote('insertNode', wiseLinkElement);
+    angular
+      .element(document.querySelector(`#${summernoteId}`))
+      .summernote('insertNode', document.createElement('br'));
+  }
+
+  /**
+   * @param type Allowed values are 'link' or 'button'.
+   */
+  createWISELinkElement(type: string, wiseLinkNodeId: string, wiseLinkComponentId: string = '',
+      wiseLinkText: string) {
+    let wiseLinkElement: any;
+    if (type === 'link') {
+      wiseLinkElement = document.createElement('a');
+    } else {
+      wiseLinkElement = document.createElement('button');
+    }
+    wiseLinkElement.setAttribute('type', type);
+    wiseLinkElement.setAttribute('wiselink', true);
+    wiseLinkElement.setAttribute('node-id', wiseLinkNodeId);
+    if (wiseLinkComponentId != '') {
+      wiseLinkElement.setAttribute('component-id', wiseLinkComponentId);
+    }
+    wiseLinkElement.setAttribute('link-text', wiseLinkText);
+    wiseLinkElement.innerHTML = wiseLinkText;
+    return wiseLinkElement;
   }
 
   createOpenAssetChooserFunction() {
@@ -183,44 +211,6 @@ class HTMLAuthoringController extends HTMLController {
         fileName
       );
     }
-  }
-
-  createWISELinkLinkElement(wiseLinkNodeId, wiseLinkComponentId = '', wiseLinkText) {
-    const wiseLinkElement: any = document.createElement('a');
-    wiseLinkElement.innerHTML = wiseLinkText;
-    wiseLinkElement.setAttribute('wiselink', true);
-    wiseLinkElement.setAttribute('node-id', wiseLinkNodeId);
-    if (wiseLinkComponentId != '') {
-      wiseLinkElement.setAttribute('component-id', wiseLinkComponentId);
-    }
-    wiseLinkElement.setAttribute('type', 'link');
-    wiseLinkElement.setAttribute('link-text', wiseLinkText);
-    return wiseLinkElement;
-  }
-
-  createWISELinkButtonElement(wiseLinkNodeId, wiseLinkComponentId = '', wiseLinkText) {
-    const wiseLinkElement: any = document.createElement('button');
-    wiseLinkElement.innerHTML = wiseLinkText;
-    wiseLinkElement.setAttribute('wiselink', true);
-    wiseLinkElement.setAttribute('node-id', wiseLinkNodeId);
-    if (wiseLinkComponentId != '') {
-      wiseLinkElement.setAttribute('component-id', wiseLinkComponentId);
-    }
-    wiseLinkElement.setAttribute('type', 'button');
-    wiseLinkElement.setAttribute('link-text', wiseLinkText);
-    return wiseLinkElement;
-  }
-
-  injectWISELinkToPrompt(wiseLinkElement) {
-    const summernoteId = 'summernotePrompt_' + this.nodeId + '_' + this.componentId;
-    angular.element(document.querySelector(`#${summernoteId}`)).summernote('editor.restoreRange');
-    angular.element(document.querySelector(`#${summernoteId}`)).summernote('editor.focus');
-    angular
-      .element(document.querySelector(`#${summernoteId}`))
-      .summernote('insertNode', wiseLinkElement);
-    angular
-      .element(document.querySelector(`#${summernoteId}`))
-      .summernote('insertNode', document.createElement('br'));
   }
 
   summernotePromptHTMLChanged() {
