ce23bf6f9283f2df577478f4dc1d05021425181b
Geoffrey Kwan <geoffreykwan@gmail.com>
2020-10-07 18:55:34 -0400

    Added log output to the updateWAR.sh script.

--- updateWAR.sh
+++ updateWAR.sh
@@ -8,26 +8,35 @@ export QA_BUILD_DIR=$HOME/build/qa
 export PROD_BUILD_DIR=$HOME/build/prod
 export WISE_BUILD_DIR=$BUILD_DIR/WISE
 
-echo DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME
+echo DEPLOYMENT_GROUP_NAME=$DEPLOYMENT_GROUP_NAME >> update.log
 
 if [[ $DEPLOYMENT_GROUP_NAME == "" ]]; then
-  echo "Requires DEPLOYMENT_GROUP_NAME to be set"
+  echo "Requires DEPLOYMENT_GROUP_NAME to be set" >> update.log
   exit 1
 fi
 
 sudo service tomcat stop
+echo "Stopped Tomcat" >> update.log
 rm -rf $CATALINA_BASE/webapps/ROOT.war
 rm -rf $CATALINA_BASE/webapps/ROOT
+echo "Removed ROOT.war and ROOT" >> update.log
 
 if [[ $DEPLOYMENT_GROUP_NAME == "qa-deployment-group" ]]; then
+    echo "Moving target/wise.war to $QA_BUILD_DIR" >> update.log
     mv target/wise.war $QA_BUILD_DIR
+    echo "Changing to QA_BUILD_DIR directory" >> update.log
     cd $QA_BUILD_DIR
 elif [[ $DEPLOYMENT_GROUP_NAME == "prod-deployment-group" ]]; then
+    echo "Moving target/wise.war to $PROD_BUILD_DIR" >> update.log
     mv target/wise.war $PROD_BUILD_DIR
+    echo "Changing to PROD_BUILD_DIR directory" >> update.log
     cd $PROD_BUILD_DIR
 fi
 
 zip -g wise.war WEB-INF/classes/application.properties
+echo "Added application.properties to wise.war" >> update.log
 mv wise.war $CATALINA_BASE/webapps/ROOT.war
+echo "Moved wise.war to ROOT.war" >> update.log
 cp WEB-INF/classes/application.properties $CATALINA_BASE/webapps/legacy/WEB-INF/classes/application.properties
 sudo service tomcat start
+echo "Started Tomcat" >> update.log
