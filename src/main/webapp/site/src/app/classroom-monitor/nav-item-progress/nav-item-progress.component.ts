219a2eb6ce4b22ac395d72f8a173744a9e82717e
Hiroki Terashima <honchikun@gmail.com>
2020-12-14 14:45:48 -0800

    Upgraded NavItemProgress to Angular component. #2842

---
+++ src/main/webapp/site/src/app/classroom-monitor/nav-item-progress/nav-item-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: 'nav-item-progress',
+  styleUrls: ['nav-item-progress.component.scss'],
+  templateUrl: 'nav-item-progress.component.html'
+})
+export class NavItemProgressComponent {
+
+  @Input()
+  nodeCompletion: string;
+
+  @Input()
+  period: any;
+}
