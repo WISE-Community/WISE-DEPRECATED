4bdc553ab217f64fbe8e41c9e3b509e10722f8d4
Geoffrey Kwan <geoffreykwan@gmail.com>
2021-01-21 12:29:14 -0500

    Upgraded Draw authoring to Angular. #2889

--- src/main/webapp/wise5/components/draw/drawAuthoringComponentModule.ts
+++ src/main/webapp/wise5/components/draw/drawAuthoringComponentModule.ts
@@ -1,15 +1,18 @@
 'use strict';
 
 import * as angular from 'angular';
-import { downgradeInjectable } from '@angular/upgrade/static';
+import { downgradeComponent, downgradeInjectable } from '@angular/upgrade/static';
 import { DrawService } from './drawService';
-import DrawAuthoring from './drawAuthoring';
 import { EditDrawAdvancedComponent } from './edit-draw-advanced/edit-draw-advanced.component';
+import { DrawAuthoring } from './draw-authoring/draw-authoring.component';
 
 const drawAuthoringComponentModule = angular
   .module('drawAuthoringComponentModule', ['pascalprecht.translate'])
   .service('DrawService', downgradeInjectable(DrawService))
-  .component('drawAuthoring', DrawAuthoring)
+  .directive(
+    'drawAuthoring',
+    downgradeComponent({ component: DrawAuthoring }) as angular.IDirectiveFactory
+  )
   .component('editDrawAdvanced', EditDrawAdvancedComponent)
   .config([
     '$translatePartialLoaderProvider',
